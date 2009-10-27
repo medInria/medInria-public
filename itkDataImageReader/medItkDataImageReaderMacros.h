2fc628685796954edf1bf3b09867cec372989d9a
Pierre Fillard <Pierre.Fillard@inria.fr>
2009-10-27 17:06:51 +0100

    ITK readers do not support reading multiple files. If a QStringList is passed as argument, it will check where its cardinality is 1. If not, it will return false.

--- itkDataImageReader/medItkDataImageReaderMacros.h
+++ itkDataImageReader/medItkDataImageReaderMacros.h
@@ -26,29 +26,31 @@
   {									\
     return QStringList() << "itkDataImage"#suffix;			\
   }									\
-  bool itkDataImage##suffix##Reader::canRead (QString path)		\
-  {									\
-    return this->canRead ( QStringList() << path );			\
-  }									\
   bool itkDataImage##suffix##Reader::canRead (QStringList paths)	\
+  {									\
+    if (paths.count()==1)						\
+      return this->canRead (paths[0]);					\
+    return false;							\
+  }									\
+  bool itkDataImage##suffix##Reader::canRead (QString path)		\
   {									\
     typedef type                             PixelType;			\
     typedef itk::Image<PixelType, dimension> ImageType;			\
     typedef itk::ImageFileReader<ImageType> ReaderType;			\
-    for( int i=0; i<paths.count(); i++) {				\
-      ReaderType::Pointer reader = ReaderType::New();			\
-      reader->SetFileName (paths[i].toAscii().constData());		\
-      try{								\
-	reader->GenerateOutputInformation();				\
-      }									\
-      catch(itk::ExceptionObject &ex) {					\
-	std::cerr << ex << std::endl;					\
-	return false;							\
-      }									\
-      if (reader->GetImageIO()->GetComponentType()!=itk::ImageIOBase::itkcomponent || \
-	  reader->GetImageIO()->GetPixelType()!=itk::ImageIOBase::itktype) \
-	return false;							\
+    ReaderType::Pointer reader = ReaderType::New();			\
+    reader->SetFileName (path.toAscii().constData());			\
+    try									\
+    {									\
+      reader->GenerateOutputInformation();				\
     }									\
+    catch(itk::ExceptionObject &ex) {					\
+      std::cerr << ex << std::endl;					\
+      return false;							\
+    }									\
+    if ( strcmp (reader->GetImageIO()->GetNameOfClass(), "GDCMImageIO")==0 || \
+	 reader->GetImageIO()->GetComponentType()!=itk::ImageIOBase::itkcomponent || \
+	 reader->GetImageIO()->GetPixelType()!=itk::ImageIOBase::itktype) \
+      return false;							\
     return true;							\
   }									\
   void itkDataImage##suffix##Reader::readInformation (QString path)	\
@@ -62,9 +64,9 @@
   }									\
   bool itkDataImage##suffix##Reader::read(QStringList paths)		\
   {									\
-    if (!paths.count())							\
-      return false;							\
-    return this->read (paths[0]);					\
+    if (paths.count()==1)						\
+      return this->read (paths[0]);					\
+    return false;							\
   }									\
   bool itkDataImage##suffix##Reader::read(QString path)			\
   {									\
