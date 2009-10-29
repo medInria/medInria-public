6c8a1bccb323daf10520929f03e9de9a7144dc42
Pierre Fillard <Pierre.Fillard@inria.fr>
2009-10-29 17:04:29 +0100

    Commented the unregister lines which seem to destroy data generated by plugins before their destructors are called, resulting in a crash

--- app/medular/main.cpp
+++ app/medular/main.cpp
@@ -77,8 +77,8 @@ int main(int argc, char *argv[])
     
     // Uninitialize managers
 
-    dtkPluginManager::instance()->uninitialize();
-    dtkScriptManager::instance()->uninitialize();
+    //dtkPluginManager::instance()->uninitialize();
+    //dtkScriptManager::instance()->uninitialize();
     
     return status;
 }
