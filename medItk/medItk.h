b761ee0e6ffacb1244a231552813adb425b2f4bc
unknown <pf219905@.intra.cea.fr>
2009-10-30 13:53:00 +0100

    Fixed medItk on WIN32

--- medItk/medItk.h
+++ medItk/medItk.h
@@ -1,5 +1,5 @@
-#ifndef _dtk_Itk_h_
-#define _dtk_Itk_h_
+#ifndef _med_Itk_h_
+#define _med_Itk_h_
 
 
 #include <itkImage.h>
@@ -13,35 +13,35 @@
 
 namespace itk
 {
-  extern template MEDITK_EXPORT class Vector<double, 2>;
-  extern template MEDITK_EXPORT class Vector<double, 3>;
-  extern template MEDITK_EXPORT class Vector<unsigned char, 3>;
+  extern template class MEDITK_EXPORT Vector<double, 2>;
+  extern template class MEDITK_EXPORT Vector<double, 3>;
+  extern template class MEDITK_EXPORT Vector<unsigned char, 3>;
 
-  extern template MEDITK_EXPORT class Point<double, 2>;
-  extern template MEDITK_EXPORT class Point<double, 3>;
+  extern template class MEDITK_EXPORT Point<double, 2>;
+  extern template class MEDITK_EXPORT Point<double, 3>;
 
-  extern template MEDITK_EXPORT class RGBPixel<unsigned char>;
+  extern template class MEDITK_EXPORT RGBPixel<unsigned char>;
   
-  extern template MEDITK_EXPORT class ImageBase<3>;
+  extern template class MEDITK_EXPORT ImageBase<3>;
 
-  extern template MEDITK_EXPORT class Image <double, 3>;
-  extern template MEDITK_EXPORT class Image <float, 3>;
-  extern template MEDITK_EXPORT class Image <long, 3>;
-  extern template MEDITK_EXPORT class Image <unsigned long, 3>;
-  extern template MEDITK_EXPORT class Image <int, 3>;
-  extern template MEDITK_EXPORT class Image <unsigned int, 3>;
-  extern template MEDITK_EXPORT class Image <short, 3>;
-  extern template MEDITK_EXPORT class Image <unsigned short, 3>;
-  extern template MEDITK_EXPORT class Image <char, 3>;
-  extern template MEDITK_EXPORT class Image <unsigned char, 3>;
+  extern template class MEDITK_EXPORT Image <double, 3>;
+  extern template class MEDITK_EXPORT Image <float, 3>;
+  extern template class MEDITK_EXPORT Image <long, 3>;
+  extern template class MEDITK_EXPORT Image <unsigned long, 3>;
+  extern template class MEDITK_EXPORT Image <int, 3>;
+  extern template class MEDITK_EXPORT Image <unsigned int, 3>;
+  extern template class MEDITK_EXPORT Image <short, 3>;
+  extern template class MEDITK_EXPORT Image <unsigned short, 3>;
+  extern template class MEDITK_EXPORT Image <char, 3>;
+  extern template class MEDITK_EXPORT Image <unsigned char, 3>;
   
-  extern template MEDITK_EXPORT class Image <RGBPixel<unsigned char> , 3>;
-  extern template MEDITK_EXPORT class Image <Vector<unsigned char, 3> , 3>;
+  extern template class MEDITK_EXPORT Image <RGBPixel<unsigned char> , 3>;
+  extern template class MEDITK_EXPORT Image <Vector<unsigned char, 3> , 3>;
   
-  extern template MEDITK_EXPORT class OrientedImage <short, 3>;
+  extern template class MEDITK_EXPORT OrientedImage <short, 3>;
 
-  extern template MEDITK_EXPORT class ImageFileWriter< Image <Vector<unsigned char, 3> , 3> >;
-  extern template MEDITK_EXPORT class ImageFileWriter< Image <RGBPixel<unsigned char> , 3> >;
+  extern template class MEDITK_EXPORT ImageFileWriter< Image <Vector<unsigned char, 3> , 3> >;
+  extern template class MEDITK_EXPORT ImageFileWriter< Image <RGBPixel<unsigned char> , 3> >;
 }
 
 #endif
