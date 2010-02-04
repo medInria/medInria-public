dec49d937ef822f0ae641d5fc21e185b2f39d4de
Pierre Fillard <Pierre.Fillard@inria.fr>
2010-02-04 17:45:17 +0100

    Attempting to define tests for plugins

---
+++ itkDataImageReader/Testing/itkMetaDataImageReaderTest.cxx
@@ -0,0 +1,51 @@
+
+#include "dtkCore/dtkPluginManager.h"
+#include "dtkCore/dtkAbstractDataFactory.h"
+#include <dtkCore/dtkAbstractDataReader.h>
+#include "dtkCore/dtkAbstractData.h"
+
+int itkMetaDataImageReaderTest (int argc, char* argv[])
+{
+  if (argc<3)
+    return EXIT_FAILURE;
+
+  // the test file
+  QString filepath = argv[2];
+  filepath += "/BrainProtonDensitySliceBorder20.mhd";
+    
+  dtkPluginManager::instance()->setPath (argv[1]);
+  dtkPluginManager::instance()->initialize();
+
+  // first method to read data
+  dtkAbstractData *data1 = dtkAbstractDataFactory::instance()->create ("itkDataImageDouble3");
+
+  if (!data1)
+      return EXIT_FAILURE;
+
+  data1->enableReader ("itkMetaDataImageReader");
+  
+  if (!data1->read(filepath))
+      return EXIT_FAILURE;
+
+  
+  // second method
+  dtkAbstractData* data2 = 0;
+
+  typedef dtkAbstractDataFactory::dtkAbstractDataTypeHandler dtkAbstractDataTypeHandler;
+  QList<dtkAbstractDataTypeHandler> readers = dtkAbstractDataFactory::instance()->readers();
+  for (int i=0; i<readers.size(); i++) {            
+      dtkAbstractDataReader* dataReader = dtkAbstractDataFactory::instance()->reader(readers[i].first, readers[i].second);
+      if (dataReader->canRead( filepath )) {
+	  dataReader->read( filepath );
+	  data2 = dataReader->data();
+	  delete dataReader;
+	  break;
+      }
+  }
+
+  if (!data2)
+      return EXIT_FAILURE;
+  
+
+  return EXIT_SUCCESS;
+}
