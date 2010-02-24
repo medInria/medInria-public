906630364b11f7770f2a2f6c46b19661a2c7b631
Pierre Fillard <Pierre.Fillard@inria.fr>
2010-02-24 18:08:49 +0100

    Thanks to the magic flag when loading plugins, we do not need a common set of ITK symbols to be able to dynamic_cast. Now, medItk only contains an adpator between Qt signals and the ITK subject/observer mechanism

--- ext/medItk/medITKProcessObjectObserver.h
+++ ext/medItk/medITKProcessObjectObserver.h
@@ -2,30 +2,74 @@
 #define _med_ITKProcessObjectObserver_h_
 
 #include <QObject>
+#include <itkCommand.h>
 
+#include "medItk/medItkExport.h"
 
-/** Helper class that interface Observers with Qt Signals */
-class QtSignalAdaptor : public QObject
+namespace itk
 {
-  Q_OBJECT
 
-public:
-  QtSignalAdaptor()
-    {
-    }
-
-  virtual ~QtSignalAdaptor() {}
+  class QtSignalAdaptor;
 
 
-  void EmitSignal(int value)
-    {
-    emit Signal(value);
-    }
+  class MEDITK_EXPORT medProcessObjectObserver : public Command
+  {
+  public:
+    typedef medProcessObjectObserver       Self;
+    typedef itk::Command                   Superclass;
+    typedef itk::SmartPointer<Self>        Pointer;
+    typedef itk::SmartPointer<const Self>  ConstPointer;
+    
+    itkTypeMacro(medProcessObjectObserver, Command);
+    itkNewMacro (Self);
+    
+    void Execute(itk::Object *caller, const itk::EventObject &event);
+    void Execute(const itk::Object *caller, const itk::EventObject &event);
+    
+    void SetQtSignalAdaptor (QtSignalAdaptor *adaptor)
+    { this->adaptor = adaptor; }
+    QtSignalAdaptor *GetQtSignalAdaptor (void) const
+    { return this->adaptor; }
+    
+  protected:
+    medProcessObjectObserver();
+    ~medProcessObjectObserver();
+    
+  private:
+    QtSignalAdaptor* adaptor;
+    
+  };
+
   
-signals:
-  void Signal(int);
-  
-};
+  /** Helper class that interface Observers with Qt Signals */
+  class MEDITK_EXPORT QtSignalAdaptor : public QObject
+  {
+    Q_OBJECT
 
+  public:
+
+    typedef medProcessObjectObserver CommandType;
+    
+    QtSignalAdaptor();
+    virtual ~QtSignalAdaptor();
+
+    CommandType *GetCommand(void)
+    { return this->command; }
+    
+    void EmitSignal(int value)
+    {
+      emit Signal(value);
+    }
+    
+  signals:
+      void Signal(int);
+    
+  private:
+      CommandType::Pointer command;
+    
+  };
+  
+  
+} // end of namespace
 
 #endif
