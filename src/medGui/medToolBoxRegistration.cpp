4023655d0ab406c4be24991f4b878f85c31992cc
Julien Wintz <jwintz@sophia.inria.fr>
2010-03-05 12:51:27 +0100

    Fixed signal/slots signature

--- src/medGui/medToolBoxRegistration.cpp
+++ src/medGui/medToolBoxRegistration.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Fri Feb 19 09:06:02 2010 (+0100)
  * Version: $Id$
- * Last-Updated: Fri Mar  5 10:26:51 2010 (+0100)
+ * Last-Updated: Fri Mar  5 12:40:37 2010 (+0100)
  *           By: Julien Wintz
- *     Update #: 187
+ *     Update #: 193
  */
 
 /* Commentary: 
@@ -47,10 +47,10 @@ public:
 
 medToolBoxRegistration::medToolBoxRegistration(QWidget *parent) : medToolBox(parent), d(new medToolBoxRegistrationPrivate)
 {
-	d->fuseView = dtkAbstractViewFactory::instance()->create("v3dView");
-	if (d->fuseView)
-		d->fuseView->enableInteractor("v3dViewFuseInteractor");
-	
+    d->fuseView = dtkAbstractViewFactory::instance()->create("v3dView");
+    if (d->fuseView)
+        d->fuseView->enableInteractor("v3dViewFuseInteractor");
+    
     // Process page
 
     QWidget *processPage = new QWidget;
@@ -71,7 +71,7 @@ medToolBoxRegistration::medToolBoxRegistration(QWidget *parent) : medToolBox(par
     processLayout->addLayout(processDropSiteLayout);
     processLayout->addWidget(processRunButton);
 
-	connect(processRunButton, SIGNAL(clicked()), this, SLOT(run()));
+    connect(processRunButton, SIGNAL(clicked()), this, SLOT(run()));
 
     // Layout page
 
@@ -79,7 +79,7 @@ medToolBoxRegistration::medToolBoxRegistration(QWidget *parent) : medToolBox(par
 
     QPushButton *layoutButtonCompare = new QPushButton("Compare", layoutPage);
     layoutButtonCompare->setCheckable(true);
-    layoutButtonCompare->setChecked(false);
+    layoutButtonCompare->setChecked(true);
 
     QPushButton *layoutButtonFuse = new QPushButton("Fuse", layoutPage);
     layoutButtonFuse->setCheckable(true);
@@ -91,40 +91,42 @@ medToolBoxRegistration::medToolBoxRegistration(QWidget *parent) : medToolBox(par
     layoutButtonGroup->setExclusive(true);
 
     QSlider *layoutFuseSlider = new QSlider(Qt::Horizontal, layoutPage);
-	layoutFuseSlider->setRange(1, 100);
-	layoutFuseSlider->setValue(50);
-	if (d->fuseView)
-		if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
-			connect(layoutFuseSlider, SIGNAL(valueChanged(int)), interactor, SLOT(onBlendAlphaValueSet(int)));
-			connect(layoutFuseSlider, SIGNAL(valueChanged(int)), interactor, SLOT(onCheckerboardDivisionCountValueSet(int)));
-		}
+    layoutFuseSlider->setRange(1, 100);
+    layoutFuseSlider->setValue(50);
+    if (d->fuseView)
+        if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
+            connect(layoutFuseSlider, SIGNAL(valueChanged(int)), interactor, SLOT(onBlendAlphaValueSet(int)));
+            connect(layoutFuseSlider, SIGNAL(valueChanged(int)), interactor, SLOT(onCheckerboardDivisionCountValueSet(int)));
+        }
+    
+    d->blendRadio = new QRadioButton("Blend", layoutPage);
+    d->checkerboardRadio = new QRadioButton("Checkerboard", layoutPage);
+    d->blendRadio->setChecked(true);
 
-	d->blendRadio = new QRadioButton("Blend", layoutPage);
-	d->checkerboardRadio = new QRadioButton("Checkerboard", layoutPage);
-	d->blendRadio->setChecked(true);
-	QButtonGroup *radioGroup = new QButtonGroup(this);
+    QButtonGroup *radioGroup = new QButtonGroup(this);
     radioGroup->addButton(d->blendRadio);
     radioGroup->addButton(d->checkerboardRadio);
     radioGroup->setExclusive(true);	
-	QHBoxLayout *radioGroupLayout = new QHBoxLayout;
-	radioGroupLayout->addWidget(d->blendRadio);
-	radioGroupLayout->addWidget(d->checkerboardRadio);
-	
+
+    QHBoxLayout *radioGroupLayout = new QHBoxLayout;
+    radioGroupLayout->addWidget(d->blendRadio);
+    radioGroupLayout->addWidget(d->checkerboardRadio);
+    
     QHBoxLayout *layoutButtonLayout = new QHBoxLayout;
     layoutButtonLayout->addWidget(layoutButtonCompare);
     layoutButtonLayout->addWidget(layoutButtonFuse);
 
     QVBoxLayout *layoutLayout = new QVBoxLayout(layoutPage);
     layoutLayout->addLayout(layoutButtonLayout);
-	layoutLayout->addLayout(radioGroupLayout);
+    layoutLayout->addLayout(radioGroupLayout);
     layoutLayout->addWidget(layoutFuseSlider);
 
     connect(layoutButtonCompare, SIGNAL(clicked()), this, SIGNAL(setupLayoutCompare()));
     connect(layoutButtonFuse, SIGNAL(clicked()), this, SIGNAL(setupLayoutFuse()));
-	
-	connect(d->blendRadio, SIGNAL(toggled(bool)), this, SLOT(onBlendModeSet(bool)));
-	connect(d->checkerboardRadio, SIGNAL(toggled(bool)), this, SLOT(onCheckerboardModeSet(bool)));
-
+    
+    connect(d->blendRadio, SIGNAL(toggled(bool)), this, SLOT(onBlendModeSet(bool)));
+    connect(d->checkerboardRadio, SIGNAL(toggled(bool)), this, SLOT(onCheckerboardModeSet(bool)));
+    
     // /////////////////////////////////////////////////////////////////
     // Setup
     // /////////////////////////////////////////////////////////////////
@@ -146,21 +148,11 @@ medToolBoxRegistration::~medToolBoxRegistration(void)
 
 dtkAbstractView *medToolBoxRegistration::fuseView(void)
 {
-	return d->fuseView;
+    return d->fuseView;
 }
 
 void medToolBoxRegistration::run(void)
-{/*
-    dtkAbstractProcess *process = dtkAbstractProcessFactory::instance()->create("itkProcessRegistration");
-
-    if(!process) {
-        qDebug() << "No itkProcessRegistration process available";
-        return;
-    }
-*/
-    qDebug() << d->processDropSiteFixed->index();
-    qDebug() << d->processDropSiteMoving->index();
-    
+{
     dtkAbstractData *fixedData = medDataManager::instance()->data(d->processDropSiteFixed->index());
 
     if(!fixedData) {
@@ -191,16 +183,15 @@ void medToolBoxRegistration::run(void)
 
     fixedView->link(movingView);
 	
-	if (d->fuseView) {
-		if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
-			interactor->setData(fixedData,  0);
-			interactor->setData(movingData, 1);
-			d->fuseView->reset();
-			d->fuseView->update();
-		}
-	}
+    if (d->fuseView) {
+        if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
+            interactor->setData(fixedData,  0);
+            interactor->setData(movingData, 1);
+            d->fuseView->reset();
+            d->fuseView->update();
+        }
+    }
 
-    //Q_UNUSED(process);
     Q_UNUSED(fixedData);
     Q_UNUSED(movingData);
     Q_UNUSED(fixedView);
@@ -209,20 +200,20 @@ void medToolBoxRegistration::run(void)
 
 void medToolBoxRegistration::onBlendModeSet(bool value)
 {
-	if (value)
-		if (d->fuseView)
-			if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
-				interactor->setProperty("FusionStyle", "blend");
-				d->fuseView->update();
-			}
+    if (value)
+        if (d->fuseView)
+            if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
+                interactor->setProperty("FusionStyle", "blend");
+                d->fuseView->update();
+            }
 }
 
 void medToolBoxRegistration::onCheckerboardModeSet(bool value)
 {
-	if (value)
-		if (d->fuseView)
-			if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
-				interactor->setProperty("FusionStyle", "checkerboard");
-				d->fuseView->update();
-			}
+    if (value)
+        if (d->fuseView)
+            if (dtkAbstractViewInteractor *interactor = d->fuseView->interactor("v3dViewFuseInteractor")) {
+                interactor->setProperty("FusionStyle", "checkerboard");
+                d->fuseView->update();
+            }
 }
