398bb842fee4badfd0148a4a8b20ed537ec3312a
Pierre Fillard <Pierre.Fillard@inria.fr>
2010-05-03 19:27:12 +0200

    Complete refactoring of fuse interactor

--- v3dView/v3dViewFuseInteractor.cpp
+++ v3dView/v3dViewFuseInteractor.cpp
@@ -9,15 +9,21 @@
 
 #include "v3dView.h"
 
-#include <vtkImageCast.h>
-#include "vtkImageFuse.h"
-
-#include "itkImageToVTKImageFilter.h"
+#include "itkVTKImageToImageFilter.h"
 #include "itkImage.h"
+#include "itkCheckerBoardImageFilter.h"
+#include "itkWeightedAddImageFilter.h"
+#include "itkCastImageFilter.h"
 
 class v3dViewFuseInteractorPrivate
 {
 public:
+
+  typedef float PixelType;
+  typedef itk::Image<PixelType, 3> ImageType;
+  typedef itk::WeightedAddImageFilter<ImageType, ImageType, ImageType>  BlendFilterType;
+  typedef itk::CheckerBoardImageFilter<ImageType>                       CheckerboardFilterType;
+  
   dtkAbstractDataImage *data1;
   dtkAbstractDataImage *data2;
 
@@ -25,33 +31,31 @@ public:
 
   v3dView *view;
   
-  vtkImageFuse* fuse;
+  BlendFilterType::Pointer        blender;
+  CheckerboardFilterType::Pointer checkerboarder;
   
 };
 
 v3dViewFuseInteractor::v3dViewFuseInteractor(): dtkAbstractViewInteractor(), d(new v3dViewFuseInteractorPrivate)
 {
-    d->data1 = dynamic_cast<dtkAbstractDataImage*>( dtkAbstractDataFactory::instance()->create("v3dDataImage") );
-    d->data2 = dynamic_cast<dtkAbstractDataImage*>( dtkAbstractDataFactory::instance()->create("v3dDataImage") );
+    d->data1 = dynamic_cast<dtkAbstractDataImage*>( dtkAbstractDataFactory::instance()->create("itkDataImageFloat3") );
+    d->data2 = dynamic_cast<dtkAbstractDataImage*>( dtkAbstractDataFactory::instance()->create("itkDataImageFloat3") );
     d->view = 0;
+
+    d->blender        = v3dViewFuseInteractorPrivate::BlendFilterType::New();
+    d->blender->SetAlpha (0.5);
     
-    d->fuse = vtkImageFuse::New();
-    d->fuse->UseClampOff();
-    d->fuse->UseColorMapsOff();
-    d->fuse->SetFuseMode ( vtkImageFuse::FUSEMODE_BLEND );
-    d->fuse->SetBlendAlpha (0.5);
-    d->fuse->SetCheckerAlpha (0.06);
+    d->checkerboarder = v3dViewFuseInteractorPrivate::CheckerboardFilterType::New();
     
-    d->output = dtkAbstractDataFactory::instance()->create("v3dDataImage");
+    d->output = dtkAbstractDataFactory::instance()->create("itkDataImageFloat3");
     if (d->output)
-        d->output->setData(d->fuse->GetOutput());
+        d->output->setData( d->blender->GetOutput() );
 
     this->addProperty("FusionStyle", QStringList() << "blend" << "checkerboard");
 }
 
 v3dViewFuseInteractor::~v3dViewFuseInteractor()
 {
-    d->fuse->Delete();
     // delete d->output;
     delete d;
     d = 0;
@@ -85,36 +89,65 @@ void v3dViewFuseInteractor::onPropertySet (QString key, QString value)
 void v3dViewFuseInteractor::onFusionStylePropertySet (QString value)
 {
     if (value=="blend")
-        d->fuse->SetFuseMode ( vtkImageFuse::FUSEMODE_BLEND );
+        d->output->setData( d->blender->GetOutput() );
 
     if (value=="checkerboard")
-        d->fuse->SetFuseMode ( vtkImageFuse::FUSEMODE_GRID );
+        d->output->setData( d->checkerboarder->GetOutput() );
+      
+     if (d->view && d->data1 && d->data1->data() &&
+	 d->data2 && d->data2->data() )
+         d->view->setData ( d->output );
 }
 
 void v3dViewFuseInteractor::onBlendAlphaValueSet (int value)
 {
-    d->fuse->SetBlendAlpha ((double)value/100.0);
-    if (d->view)
+    d->blender->SetAlpha ( (double)value/100.0 );
+
+    if (d->view) {
         d->view->update();
+    }
 }
 
 void v3dViewFuseInteractor::onBlendAlphaValueSet (double value)
 {
-    d->fuse->SetBlendAlpha (value);
-    if (d->view)
+    d->blender->SetAlpha (value);
+    
+    if (d->view) {
         d->view->update();
+    }
 }
 
 void v3dViewFuseInteractor::onCheckerboardDivisionCountValueSet (int value)
 {
-    d->fuse->SetCheckerAlpha ((double)value/100.0);
+    v3dViewFuseInteractorPrivate::CheckerboardFilterType::PatternArrayType array;
+
+    if (d->data1) {
+      if ( value>d->data1->xDimension() )
+	value = d->data1->xDimension();
+      
+      if ( value>d->data1->yDimension() )
+	value = d->data1->yDimension();
+      
+      if ( value>d->data1->zDimension() )
+	value = d->data1->zDimension();  
+    }
+
+    for (int i=0; i<3; i++)
+        array[i] = value;
+
+    d->checkerboarder->SetCheckerPattern (array);
+    
     if (d->view)
         d->view->update();
 }
 
 void v3dViewFuseInteractor::onCheckerboardDivisionCountValueSet (double value)
 {
-    d->fuse->SetCheckerAlpha (value);
+    v3dViewFuseInteractorPrivate::CheckerboardFilterType::PatternArrayType array;
+    for (int i=0; i<3; i++)
+      array[i] = (int)(value*100.0);
+    d->checkerboarder->SetCheckerPattern (array);
+    
     if (d->view)
         d->view->update();
 }
@@ -123,217 +156,242 @@ void v3dViewFuseInteractor::setData(dtkAbstractData *data, int channel)
 {
     if (!data || channel<0 || channel>1 || !data->data())
         return;
-    
         
     if (data->description()==tr("itkDataImageChar3")) {
       typedef char ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New(); 
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
-      
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
+
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
     else if (data->description()==tr("itkDataImageUChar3")) {
       typedef unsigned char ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
-      
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
+
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageShort3")) {
       typedef short ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }      
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageUShort3")) {
       typedef unsigned short ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageInt3")) {
       typedef int ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageUInt3")) {
       typedef unsigned int ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageLong3")) {
       typedef long ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageULong3")) {
       typedef unsigned long ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageFloat3")) {
       typedef float ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
 
     else if (data->description()==tr("itkDataImageDouble3")) {
       typedef double ScalarType;
       typedef itk::Image<ScalarType, 3> ImageType;
-      typedef itk::ImageToVTKImageFilter<ImageType> ConverterType;
-      ConverterType::Pointer converter = ConverterType::New();
-      converter->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
-      converter->Update();
 
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput (converter->GetOutput());
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+      typedef itk::CastImageFilter<ImageType, v3dViewFuseInteractorPrivate::ImageType> CasterType;
+
+      CasterType::Pointer caster = CasterType::New();
+      caster->SetInput ( dynamic_cast<ImageType*>( (itk::Object*)(data->data()) ) );
+      try {
+	caster->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
           d->data1->setData ( caster->GetOutput() );
       else if (channel==1)
           d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
     }
-    else if (data->description()==tr("vtkDataImage")) {
-      vtkImageCast *caster = vtkImageCast::New();
-      caster->SetInput ( dynamic_cast<vtkImageData*>( (vtkObject*)(data->data()) ) );
-      caster->SetOutputScalarTypeToFloat ();
-      caster->GetOutput()->Update();
+    else if (data->description()==tr("v3dDataImage")) {
+      typedef itk::VTKImageToImageFilter<v3dViewFuseInteractorPrivate::ImageType> ConverterType;
+      ConverterType::Pointer converter = ConverterType::New();
+      
+      converter->SetInput ( dynamic_cast<vtkImageData*>( (vtkObject*)(data->data()) ) );
+      try {
+	converter->Update();
+      }
+      catch (itk::ExceptionObject &e) {
+	qDebug() << e.GetDescription();
+	return;
+      }
       
       if (channel==0)
-          d->data1->setData ( caster->GetOutput() );
+          d->data1->setData ( converter->GetOutput() );
       else if (channel==1)
-          d->data2->setData ( caster->GetOutput() );
-      caster->Delete();
+          d->data2->setData ( converter->GetOutput() );
     }
     else
       return;
@@ -348,12 +406,34 @@ void v3dViewFuseInteractor::setData(dtkAbstractData *data, int channel)
 	return;
       }
 
+
+        v3dViewFuseInteractorPrivate::ImageType::Pointer input1 = dynamic_cast<v3dViewFuseInteractorPrivate::ImageType*>(static_cast<itk::Object*>(d->data1->data()));
+	v3dViewFuseInteractorPrivate::ImageType::Pointer input2 = dynamic_cast<v3dViewFuseInteractorPrivate::ImageType*>(static_cast<itk::Object*>(d->data2->data()));
       
-        d->fuse->SetInput (0, dynamic_cast<vtkImageData*>(static_cast<vtkObject*>(d->data1->data())) );
-	d->fuse->SetInput (1, dynamic_cast<vtkImageData*>(static_cast<vtkObject*>(d->data2->data())) );
+        d->blender->SetInput1 ( input1 );
+	d->blender->SetInput2 ( input2 );
 	
-	d->fuse->GetOutput()->SetUpdateExtentToWholeExtent();      
-        d->fuse->GetOutput()->Update();
+	d->checkerboarder->SetInput1 ( input1 );
+	d->checkerboarder->SetInput2 ( input2 );
+
+	v3dViewFuseInteractorPrivate::CheckerboardFilterType::PatternArrayType pattern = d->checkerboarder->GetCheckerPattern();
+	if ((int)pattern[0]>d->data1->xDimension() )
+	    pattern[0] = d->data1->xDimension();
+	if ((int)pattern[1]>d->data1->yDimension() )
+	    pattern[1] = d->data1->yDimension();
+	if ((int)pattern[2]>d->data1->zDimension() )
+	    pattern[2] = d->data1->zDimension();
+
+	d->checkerboarder->SetCheckerPattern ( pattern );
+
+	try {
+	    d->blender->Update();
+	    d->checkerboarder->Update();
+	}
+	catch (itk::ExceptionObject &e) {
+	  qDebug() << e.GetDescription();
+	  return;
+	}
 
 	if (d->data1->hasMetaData ("PatientName") && d->data2->hasMetaData ("PatientName"))
 	    d->output->addMetaData ("PatientName",
@@ -377,7 +457,9 @@ void v3dViewFuseInteractor::enable(void)
     if (this->enabled())
         return;
 	
-    if (d->view && d->data1 && d->data2) {
+    if (d->view &&
+	d->data1 && d->data1->data() &&
+	d->data2 && d->data2->data()) {
         d->view->setData ( d->output );
     }
     dtkAbstractViewInteractor::enable();
