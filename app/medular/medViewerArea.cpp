af800b8334ec22449d514e001f8e18ed7bdf7321
Julien Wintz <jwintz@sophia.inria.fr>
2010-05-13 15:09:00 +0200

    Splitting viewer related classes.

--- app/medular/medViewerArea.cpp
+++ app/medular/medViewerArea.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Fri Sep 18 12:43:06 2009 (+0200)
  * Version: $Id$
- * Last-Updated: Wed Mar 17 18:58:32 2010 (+0100)
+ * Last-Updated: Thu May 13 14:18:39 2010 (+0200)
  *           By: Julien Wintz
- *     Update #: 770
+ *     Update #: 777
  */
 
 /* Commentary: 
@@ -18,6 +18,9 @@
  */
 
 #include "medViewerArea.h"
+#include "medViewerAreaStack.h"
+#include "medViewerConfiguration.h"
+#include "medViewerConfigurator.h"
 
 #include <dtkCore/dtkAbstractViewFactory.h>
 #include <dtkCore/dtkAbstractView.h>
@@ -48,99 +51,6 @@
 
 #include <QtGui>
 #include <QtSql>
-
-// /////////////////////////////////////////////////////////////////
-// medViewerAreaStack
-// /////////////////////////////////////////////////////////////////
-
-class medViewerAreaStackPrivate
-{
-public:
-    int id;
-
-    medViewContainerSingle *container_single;
-    medViewContainerMulti *container_multi;
-    medViewContainerCustom *container_custom;
-    medViewContainer *container_registration_compare;
-    medViewContainer *container_registration_fuse;
-};
-
-medViewerAreaStack::medViewerAreaStack(QWidget *parent) : QStackedWidget(parent), d(new medViewerAreaStackPrivate)
-{
-    d->container_single = new medViewContainerSingle(this);
-    d->container_multi = new medViewContainerMulti(this);
-    d->container_custom = new medViewContainerCustom(this);
-    d->container_registration_compare = new medViewContainerCustom(this);
-    d->container_registration_compare->split(1, 2);
-    d->container_registration_fuse = new medViewContainerCustom(this);
-
-    this->addWidget(d->container_single);
-    this->addWidget(d->container_multi);
-    this->addWidget(d->container_custom);
-    this->addWidget(d->container_registration_compare);
-    this->addWidget(d->container_registration_fuse);
-
-    this->setCurrentIndex(0);
-
-    connect(d->container_single, SIGNAL(focused(dtkAbstractView*)), this, SIGNAL(focused(dtkAbstractView*)));
-    connect(d->container_multi,  SIGNAL(focused(dtkAbstractView*)), this, SIGNAL(focused(dtkAbstractView*)));
-    connect(d->container_custom, SIGNAL(focused(dtkAbstractView*)), this, SIGNAL(focused(dtkAbstractView*)));
-    connect(d->container_registration_compare, SIGNAL(focused(dtkAbstractView*)), this, SIGNAL(focused(dtkAbstractView*)));
-    connect(d->container_registration_fuse, SIGNAL(focused(dtkAbstractView*)), this, SIGNAL(focused(dtkAbstractView*)));
-
-    connect(d->container_single, SIGNAL(dropped(const medDataIndex&)), this, SIGNAL(dropped(const medDataIndex&)));
-    connect(d->container_multi,  SIGNAL(dropped(const medDataIndex&)), this, SIGNAL(dropped(const medDataIndex&)));
-    connect(d->container_custom, SIGNAL(dropped(const medDataIndex&)), this, SIGNAL(dropped(const medDataIndex&)));
-    connect(d->container_registration_compare, SIGNAL(dropped(const medDataIndex&)), this, SIGNAL(dropped(const medDataIndex&)));
-    connect(d->container_registration_fuse, SIGNAL(dropped(const medDataIndex&)), this, SIGNAL(dropped(const medDataIndex&)));
-}
-
-medViewerAreaStack::~medViewerAreaStack(void)
-{
-    delete d;
-
-    d = NULL;
-}
-
-void medViewerAreaStack::setPatientId(int id)
-{
-    d->id = id;
-}
-
-medViewContainer *medViewerAreaStack::current(void)
-{
-    if(this->currentIndex() == 0)
-        return d->container_single;
-
-    if(this->currentIndex() == 1)
-        return d->container_multi;
-
-    if(this->currentIndex() == 2)
-        return d->container_custom;
-	
-    if(this->currentIndex() == 3)
-        return d->container_registration_compare;
-    
-    if(this->currentIndex() == 4)
-        return d->container_registration_fuse;
-
-    return NULL;
-}
-
-medViewContainer *medViewerAreaStack::single(void)
-{
-    return d->container_single;
-}
-
-medViewContainer *medViewerAreaStack::multi(void)
-{
-    return d->container_multi;
-}
-
-medViewContainer *medViewerAreaStack::custom(void)
-{
-    return d->container_custom;
-}
 
 // /////////////////////////////////////////////////////////////////
 // medViewerArea
@@ -618,105 +528,3 @@ void medViewerArea::setupLayoutFuse(void)
         d->view_stacks.value(d->patientToolBox->patientIndex())->current()->setFocus(Qt::MouseFocusReason);
 	}
 }
-
-// /////////////////////////////////////////////////////////////////
-// medViewerConfiguration
-// /////////////////////////////////////////////////////////////////
-
-class medViewerConfigurationPrivate
-{
-public:
-    medViewerArea *parent;
-
-    int container_index;
-
-    QHash<medToolBox *, bool> toolboxes;
-};
-
-medViewerConfiguration::medViewerConfiguration(medViewerArea *parent) : QObject(), d(new medViewerConfigurationPrivate)
-{
-    d->parent = parent;
-}
-
-medViewerConfiguration::~medViewerConfiguration(void)
-{
-    delete d;
-
-    d = NULL;
-}
-
-void medViewerConfiguration::setup(void)
-{
-    foreach(medToolBox *toolbox, d->toolboxes.keys())
-        toolbox->setVisible(d->toolboxes.value(toolbox));
-
-    d->parent->setStackIndex(d->container_index);
-}
-
-void medViewerConfiguration::setdw(void)
-{
-    foreach(medToolBox *toolbox, d->toolboxes.keys())
-        toolbox->setVisible(!(d->toolboxes.value(toolbox)));
-
-    // TODO: update container index to save current state
-}
-
-void medViewerConfiguration::attach(int index)
-{
-    d->container_index = index;
-}
-
-void medViewerConfiguration::attach(medToolBox *toolbox, bool visible)
-{
-    d->toolboxes.insert(toolbox, visible);
-}
-
-// /////////////////////////////////////////////////////////////////
-// medViewerConfigurator
-// /////////////////////////////////////////////////////////////////
-
-class medViewerConfiguratorPrivate
-{
-public:
-    QHash<QString, medViewerConfiguration *> configurations;
-
-    QString current;
-};
-
-medViewerConfigurator *medViewerConfigurator::instance(void)
-{
-    if(!s_instance)
-        s_instance = new medViewerConfigurator;
-
-    return s_instance;
-}
-
-medViewerConfigurator::medViewerConfigurator(void) : QObject(), d(new medViewerConfiguratorPrivate)
-{
-
-}
-
-medViewerConfigurator::~medViewerConfigurator(void)
-{
-    delete d;
-
-    d = NULL;
-}
-
-void medViewerConfigurator::addConfiguration(QString name, medViewerConfiguration *configuration)
-{
-    d->configurations.insert(name, configuration);
-}
-
-void medViewerConfigurator::setConfiguration(QString name)
-{
-    if(!d->current.isNull())
-        d->configurations.value(d->current)->setdw();
-
-    if (d->configurations.keys().contains(name))
-        d->configurations.value(name)->setup();
-
-    d->current = name;
-}
-
-medViewerConfigurator *medViewerConfigurator::s_instance = NULL;
