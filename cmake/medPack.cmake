ebb8b09ddc2d4185018582b35aa82ee062640c7e
Benoît Bleuzé <bbleuze@sophia.inria.fr>
2010-05-31 15:57:47 +0200

    includes dtk version in deb pkgs

--- cmake/medPack.cmake
+++ cmake/medPack.cmake
@@ -1,2 +1,2 @@
 #add dependancy on dtk for debian packages:
-set (CPACK_DEBIAN_PACKAGE_DEPENDS "dtk") 
+set (CPACK_DEBIAN_PACKAGE_DEPENDS "dtk (>= ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR})") 
