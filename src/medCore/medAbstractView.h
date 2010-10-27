77ee83389a8960b0f57a95024f2320bad276b7e4
Julien Wintz <jwintz@is-master.inria.fr>
2010-10-27 16:43:30 +0200

    Adding head and finger tracking support.

--- src/medCore/medAbstractView.h
+++ src/medCore/medAbstractView.h
@@ -1,15 +1,36 @@
+/* medAbstractView.h --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008 - Julien Wintz, Inria.
+ * Created: Thu Oct 21 19:54:36 2010 (+0200)
+ * Version: $Id$
+ * Last-Updated: Tue Oct 26 10:46:04 2010 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 9
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
 
 #ifndef MEDABSTRACTVIEW_H
 #define MEDABSTRACTVIEW_H
+
 #include <dtkCore/dtkAbstractView.h>
+
 #include "medCoreExport.h"
 
 class MEDCORE_EXPORT medAbstractView: public dtkAbstractView
 {
     Q_OBJECT
+
 public:
-    medAbstractView(medAbstractView * parent = 0);
-    medAbstractView(const medAbstractView& view);
+             medAbstractView(medAbstractView * parent = 0);
+             medAbstractView(const medAbstractView& view);
     virtual ~medAbstractView(void){}
 
     virtual void setColorLookupTable(int min_range,
@@ -18,5 +39,8 @@ public:
                                      const int & table);
 
     virtual void setColorLookupTable(QList<double>scalars,QList<QColor>colors);
+
+    virtual QWidget *receiverWidget(void);
 };
+
 #endif
