4193c9463f99e4009e589d5622e3d8c0c89ec33f
Pierre Fillard <Pierre.Fillard@inria.fr>
2011-01-07 15:01:47 +0100

    Preparing medinria-plugins for ADT NT

--- dcmtkDatabasePacs/dcmtkPacsKey.cpp
+++
@@ -1,8 +0,0 @@
-#include "dcmtkPacsKey.h"
-
-void dcmtkPacsKey::convert( dcmtkKey* key )
-{
-    elem = key->elem;
-    group = key->group;
-    value = QString::fromStdString(key->value);
-}
--- dcmtkDatabasePacs/medDcmtk/dcmtkBaseScp.cpp
+++
@@ -1,8 +0,0 @@
-#include "dcmtkBaseScp.h"
-
-
-void dcmtkBaseScp::setConnectionParams(const char* ourTitle, const char* ourIP, unsigned short ourPort)
-{
-     opt_respondingAETitle = ourTitle;
-     opt_port = ourPort;
-}
