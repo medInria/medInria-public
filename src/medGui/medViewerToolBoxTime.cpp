812b94e0dc342355e09d463ba1d9fda4ca04e23e
Nicolas Toussaint <nicolas.toussaint@sophia.inria.fr>
2011-02-16 15:28:12 +0000

    adding the missing files

---
+++ src/medGui/medViewerToolBoxTime.cpp
@@ -0,0 +1,204 @@
+#include "medViewerToolBoxTime.h"
+
+
+#include <dtkCore/dtkAbstractView.h>
+#include <dtkCore/dtkLog.h>
+
+#include <medGui/medToolBoxTab.h>
+#include <medCore/med4DAbstractViewInteractor.h>
+#include <QList>
+
+#include <cmath>
+
+class medViewerToolBoxTimePrivate
+{
+public:
+  QSlider     *timeSlider;
+  QPushButton *playSequencesPushButton;
+  QList<dtkAbstractView*> views;
+  QList<med4DAbstractViewInteractor*> interactors;
+  QTimeLine *timeLine;
+
+};
+
+medViewerToolBoxTime::medViewerToolBoxTime(QWidget *parent) : medToolBox(parent), d(new medViewerToolBoxTimePrivate)
+{
+  QWidget *box = new QWidget (this);
+    
+  d->timeSlider = new QSlider (Qt::Horizontal, this);
+  d->timeSlider->setRange (0, 100);
+  d->timeSlider->setValue (100);
+  //d->timeSlider->setTracking( false );
+  
+  d->playSequencesPushButton = new QPushButton("", this);
+  d->playSequencesPushButton->setIcon (QIcon (":/icons/wlww.tiff"));
+  d->playSequencesPushButton->setCheckable (true);
+  d->playSequencesPushButton->setMinimumWidth ( 20 );
+
+  d->timeLine = new QTimeLine(3000, this);
+  d->timeLine->setLoopCount(0);
+  d->timeLine->setCurveShape (QTimeLine::LinearCurve);
+  
+  connect(d->timeLine, SIGNAL(frameChanged(int)), d->timeSlider, SLOT(setValue(int)));
+  // connect(d->timeLine, SIGNAL(frameChanged(int)), this, SLOT(onTimeChanged(int)));
+
+  
+  QVBoxLayout* boxlayout = new QVBoxLayout (this);
+  boxlayout->addWidget (d->playSequencesPushButton);
+  boxlayout->addWidget (d->timeSlider);
+
+  connect(d->timeSlider, SIGNAL(sliderMoved(int)), this, SLOT(onTimeChanged(int)));
+  connect(d->timeSlider, SIGNAL(valueChanged(int)), this, SLOT(onTimeChanged(int)));
+  connect(d->playSequencesPushButton, SIGNAL(toggled(bool)), this, SLOT(onPlaySequences(bool)));
+  
+  this->setTitle("Time Management");
+  box->setLayout (boxlayout);
+  this->addWidget (box);
+
+  this->minTime = 0.0;
+  this->minTimeStep = 1.0;
+  
+}
+
+medViewerToolBoxTime::~medViewerToolBoxTime(void)
+{
+  delete d;
+  
+  d = NULL;
+}
+
+void medViewerToolBoxTime::onViewAdded (dtkAbstractView *view)
+{
+  if (!view)
+    return;
+  dtkWarning() << "medViewerToolBoxTime::onViewAdded" ;
+  
+
+  if (med4DAbstractViewInteractor *interactor = dynamic_cast<med4DAbstractViewInteractor*>(view->interactor ("v3dView4DInteractor")))
+  {
+    if (!d->views.contains (view))
+      d->views.append (view);
+    if (!d->interactors.contains (interactor))
+      d->interactors.append (interactor);
+
+    connect (view, SIGNAL ( dataAdded(dtkAbstractData*)),   this, SLOT (onDataAdded (dtkAbstractData*)));
+  }
+
+  this->UpdateRange();
+}
+
+void medViewerToolBoxTime::onDataAdded (dtkAbstractData *data)
+{
+  dtkWarning() << "medViewerToolBoxTime::onDataAdded" ;
+
+  if (!data)
+    return;
+
+  this->UpdateRange();
+}
+
+void medViewerToolBoxTime::onViewRemoved (dtkAbstractView *view)
+{
+  if (!view)
+    return;
+
+  if (med4DAbstractViewInteractor *interactor = dynamic_cast<med4DAbstractViewInteractor*>(view->interactor ("v3dView4DInteractor")))
+  {
+    d->views.removeOne (view);
+    d->interactors.removeOne (interactor);
+  }  
+
+  this->UpdateRange();
+  
+}
+
+void medViewerToolBoxTime::AddInteractor (med4DAbstractViewInteractor* interactor)
+{
+  d->interactors.removeOne (interactor);
+  d->interactors.append (interactor);
+}
+void medViewerToolBoxTime::RemoveInteractor (med4DAbstractViewInteractor* interactor)
+{
+  d->interactors.removeOne (interactor);
+}
+
+void medViewerToolBoxTime::update(dtkAbstractView *view)
+{
+  // // find all interactors
+  // QList<dtkAbstractView *> views = currentStack()->current()->views();
+  // for (unsigned int i=0; i<views.size(); i++)
+  //   this->onViewAdded (views[i]);
+  
+  // //this->AddInteractor(...)
+}
+
+void medViewerToolBoxTime::onPlaySequences (bool val)
+{
+  this->UpdateRange();
+  
+  if(val)
+    d->timeLine->start();
+  else
+    d->timeLine->stop();
+}
+
+
+void medViewerToolBoxTime::onTimeChanged (int val)
+{
+  double time = this->getTimeFromSliderValue (val);
+  
+  //  dtkWarning() << "time is "<< val;  
+  for (unsigned int i=0; i<d->interactors.size(); i++)
+  {
+    d->interactors[i]->setCurrentTime (time);
+  }
+}
+
+double medViewerToolBoxTime::getTimeFromSliderValue (unsigned int s)
+{
+  double value = this->minTime + (double)(s) * (this->minTimeStep);
+  return value;  
+}
+
+unsigned int medViewerToolBoxTime::getSliderValueFromTime (double t)
+{
+  unsigned int value = std::ceil ((t - this->minTime) / (this->minTimeStep));
+  return value;
+}
+
+
+void medViewerToolBoxTime::UpdateRange (void)
+{
+  if (!d->interactors.size())
+    return;
+  
+  double mintime = 3000;
+  double maxtime = -3000;
+  double mintimestep = 3000;
+  
+  for (unsigned int i=0; i<d->interactors.size(); i++)
+  {
+    double range[2]={0,0};
+    d->interactors[i]->sequencesRange (range);
+    qWarning() << "sequence range = " << range[0]<<" :: "<<range[1];
+    qWarning() << "sequence min time step = " << d->interactors[i]->sequencesMinTimeStep ();
+    mintimestep = std::min (mintimestep, d->interactors[i]->sequencesMinTimeStep ());
+    mintime = std::min (mintime, range[0]);
+    maxtime = std::max (maxtime, range[1]);
+  }
+
+  unsigned int numberofsteps = std::ceil ((maxtime - mintime) / (mintimestep) + 1.0);
+
+  d->timeSlider->setRange (0, numberofsteps - 1);
+  d->timeLine->setFrameRange(d->timeSlider->minimum(), d->timeSlider->maximum() );  
+  
+  this->minTime = mintime;
+  this->minTimeStep = mintimestep;
+
+  qWarning() << "numberofsteps = " << numberofsteps;
+  qWarning() << "mintime = " << mintime;
+  qWarning() << "maxtime = " << maxtime;
+  qWarning() << "mintimestep = " << mintimestep;
+  
+}
+
---
+++ src/medGui/medViewerToolBoxTime.h
@@ -0,0 +1,46 @@
+#ifndef MEDVIEWERTOOLBOXTIME_H
+#define MEDVIEWERTOOLBOXTIME_H
+
+#include <medGui/medToolBox.h>
+
+class dtkAbstractView;
+class dtkAbstractData;
+class medViewerToolBoxTimePrivate;
+class med4DAbstractViewInteractor;
+
+class medViewerToolBoxTime : public medToolBox
+{
+    Q_OBJECT
+
+public:
+     medViewerToolBoxTime(QWidget *parent = 0);
+    ~medViewerToolBoxTime(void);
+
+    void update(dtkAbstractView *view);
+
+public slots:
+    
+    void onPlaySequences(bool);
+    void onTimeChanged(int);
+    void onViewAdded   (dtkAbstractView *view);
+    void onViewRemoved (dtkAbstractView *view);
+    void onDataAdded (dtkAbstractData* data);
+
+ protected:
+
+    void AddInteractor (med4DAbstractViewInteractor* interactor);
+    void RemoveInteractor (med4DAbstractViewInteractor* interactor);
+
+    void UpdateRange (void);
+
+    double getTimeFromSliderValue (unsigned int);
+    unsigned int getSliderValueFromTime (double);
+    
+private:
+    medViewerToolBoxTimePrivate *d;
+    double minTime;
+    double minTimeStep;
+    
+};
+
+#endif
