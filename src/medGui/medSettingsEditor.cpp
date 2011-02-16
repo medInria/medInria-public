34ee8d7c325b2edf9355e5dcd8ba6542617377e3
Benoit Bleuze <benoit.bleuze@inria.fr>
2011-02-16 11:24:07 +0100

    added an advanced settingsEditor

--- src/medGui/medSettingsEditor.cpp
+++ src/medGui/medSettingsEditor.cpp
@@ -4,6 +4,8 @@
 #include <QtGui>
 #include <QWidget>
 
+#include <dtkGui/dtkSettingsEditor.h>
+
 #include <medCore/medMessageController.h>
 #include <medGui/medSystemSettingsWidget.h>
 #include <medGui/medSettingsWidgetFactory.h>
@@ -12,16 +14,24 @@ class medSettingsEditorPrivate {
 
 public:
   QWidget* parent;
+  QStackedWidget * stack;
   QTabWidget* tabWidget;
+  dtkSettingsEditor *editor;
+  QPushButton* advanced;
+  QPushButton* reset;
+  QPushButton* cancel;
   QHash<QString,medSettingsWidget*> settingsWidgets;
   //medSettingsWidget * systemSettingsWidget;
 
   medSettingsEditorPrivate();
   ~medSettingsEditorPrivate();
+
+  void read();
 };
 
 medSettingsEditorPrivate::medSettingsEditorPrivate()
 {
+
 }
 
 medSettingsEditorPrivate::~medSettingsEditorPrivate()
@@ -35,9 +45,17 @@ medSettingsEditor::medSettingsEditor(QWidget *parent) :
     QVBoxLayout * vLayout = new QVBoxLayout(this);
     setLayout(vLayout);
     this->setFixedSize(QSize(500,500));
+    d->stack = new QStackedWidget(this);
     d->tabWidget = new QTabWidget (this);
     d->tabWidget->setTabPosition(QTabWidget::West);
     d->tabWidget->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+
+    //use stackedWidget to display the advanced or normal editor
+    d->stack->addWidget(d->tabWidget);
+
+    d->editor = new dtkSettingsEditor(this);
+    d->stack->addWidget(d->editor);
+
 
     medSettingsWidgetFactory * settingsFactory =
             medSettingsWidgetFactory::instance();
@@ -59,59 +77,84 @@ medSettingsEditor::medSettingsEditor(QWidget *parent) :
         }
     }
 
-    this->layout()->addWidget(d->tabWidget);
+    //fill in widgets with the settings
+    d->read();
 
+    this->layout()->addWidget(d->stack);
+
+    //advanced button
+    d->advanced = new QPushButton (tr("Advanced"),this);
+    connect(d->advanced,SIGNAL(clicked()),
+            this,SLOT(onAdvancedClicked()));
     //save button
     QPushButton * save = new QPushButton (tr("Save"),this);
     connect(save,SIGNAL(clicked()),
             this,SLOT(onSaveClicked()));
 
     //cancel button
-    QPushButton * cancel = new QPushButton (tr("Cancel"),this);
-    connect(cancel,SIGNAL(clicked()),
+    d->cancel = new QPushButton (tr("Cancel"),this);
+    connect(d->cancel,SIGNAL(clicked()),
             this,SLOT(onCancelClicked()));
     //reset button
-    QPushButton * reset = new QPushButton (tr("Reset"), this);
-    connect(reset,SIGNAL(clicked()),
+    d->reset = new QPushButton (tr("Reset"), this);
+    connect(d->reset,SIGNAL(clicked()),
             this,SLOT(onResetClicked()));
 
     QHBoxLayout * buttonLayout = new QHBoxLayout(this);
-    buttonLayout->addWidget(reset);
-    buttonLayout->addWidget(cancel);
+    buttonLayout->addWidget(d->advanced);
+    buttonLayout->addWidget(d->reset);
+    buttonLayout->addWidget(d->cancel);
     buttonLayout->addWidget(save);
     vLayout->addLayout(buttonLayout);
 
     connect(this,SIGNAL(showError(QObject*,const        QString&,unsigned int)),
             medMessageController::instance(),SLOT(showError (QObject*,const QString&,unsigned int)));
+    connect(this,SIGNAL(showInfo(QObject*,const        QString&,unsigned int)),
+            medMessageController::instance(),SLOT(showInfo (QObject*,const QString&,unsigned int)));
 
-    foreach(medSettingsWidget * setting, d->settingsWidgets)
+
+}
+
+void medSettingsEditorPrivate::read()
+{
+    foreach(medSettingsWidget * setting, settingsWidgets)
     {
         setting->read();
     }
 }
 
-
 void medSettingsEditor::onSaveClicked(){
 
-
-
-    foreach(medSettingsWidget * setting, d->settingsWidgets)
+    bool success = true;
+    if (!d->stack->currentIndex())
     {
-        if (!setting->validate())
+        foreach(medSettingsWidget * setting,
+                d->settingsWidgets)
         {
-            qDebug()<<"validation of section"<< setting->section() <<
-                    "failed";
-            QString error = "Error in validation of section";
-            error.append(setting->section());
-            emit (showError(this,error,3));
+            if (!setting->validate())
+            {
+                qDebug()<<"validation of section"<< setting->section() <<
+                        "failed";
+                QString error = tr("Error in validation of section ");
+                error.append(setting->section());
+                emit (showError(this,error,3000));
+                success = false;
+            }
+            else
+            {
+                //do the saving
+                setting->write();
+                qDebug()<<"validation of section"<< setting->section() <<
+                        "successful";
+            }
         }
-        else
-        {
-            //do the saving
-            setting->write();
-            qDebug()<<"validation of section"<< setting->section() <<
-                    "successful";
-        }
+    }
+    if (success)
+    {
+         emit (showInfo(this,
+                           tr("Settings successfully saved"),
+                           3000));
+        this->close();
     }
 }
 
@@ -120,8 +163,25 @@ void medSettingsEditor::onCancelClicked(){
 }
 
 void medSettingsEditor::onResetClicked(){
-    foreach(medSettingsWidget * setting, d->settingsWidgets)
-    {
-        setting->read();
+    d->read();
+}
+
+void medSettingsEditor::onAdvancedClicked()
+{
+    if (d->stack->currentIndex()){
+        //index is 1 ->advanced->default
+        d->read();
+        d->cancel->show();
+        d->reset->show();
+        d->advanced->setText(tr("Advanced"));
+        d->stack->setCurrentIndex(0);
     }
+    else{
+        d->advanced->setText(tr("Default"));
+        d->cancel->hide();
+        d->reset->hide();
+        d->editor->setSettings("inria", "medinria");
+        d->stack->setCurrentIndex(1);
+    }
+
 }
--- src/medGui/medSettingsEditor.h
+++ src/medGui/medSettingsEditor.h
@@ -22,7 +22,18 @@ signals:
     */
     void showError(QObject *sender, const QString& text,unsigned int timeout=0);
 
+    /**
+     * @brief Emits an info message for the medMessageController to display.
+     *
+     * Typically here, it is emitted when the settings are successfully saved.
+     * @param sender Should be the current Object itself.
+     * @param text The error message.
+     * @param timeout The timeout before the message disapears.
+    */
+    void showInfo(QObject *sender, const QString& text,unsigned int timeout=0);
+
 public slots:
+    void onAdvancedClicked();
     void onSaveClicked();
     void onCancelClicked();
     void onResetClicked();
