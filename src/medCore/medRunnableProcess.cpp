189e2b9ffb74a3af934b990f3c423c5d390d002e
Pierre Fillard <Pierre.Fillard@inria.fr>
2011-03-14 17:59:23 +0100

    Adding medRunnableProcess class deriving from medJobItem to run a dtkAbstractProcess in a separate thread

---
+++ src/medCore/medRunnableProcess.cpp
@@ -0,0 +1,73 @@
+/* medRunnableProcess.cpp --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008 - Julien Wintz, Inria.
+ * Created: Tue Dec 15 09:40:14 2009 (+0100)
+ * Version: $Id$
+ * Last-Updated: Tue Dec 15 09:40:15 2009 (+0100)
+ *           By: Julien Wintz
+ *     Update #: 1
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
+
+#include "medRunnableProcess.h"
+
+#include <dtkCore/dtkAbstractProcess.h>
+
+class medRunnableProcessPrivate
+{
+public:
+    dtkAbstractProcess *process;
+};
+
+
+medRunnableProcess::medRunnableProcess(void): medJobItem(), d (new medRunnableProcessPrivate)
+{
+    d->process = 0;
+}
+
+medRunnableProcess::~medRunnableProcess (void)
+{
+    delete d;
+    d = NULL;
+}
+
+void medRunnableProcess::setProcess (dtkAbstractProcess *proc)
+{
+    d->process = proc;
+
+    if (d->process) {
+	connect (d->process, SIGNAL (success()),       this, SLOT (onSuccess()),       Qt::BlockingQueuedConnection);
+	connect (d->process, SIGNAL (failure()),       this, SLOT (onFailure()),       Qt::BlockingQueuedConnection);
+	connect (d->process, SIGNAL (progressed(int)), this, SLOT (onProgressed(int)), Qt::QueuedConnection);
+    }
+}
+
+void medRunnableProcess::run (void)
+{
+    if (d->process) {
+        d->process->run();
+    }
+}
+
+void medRunnableProcess::onSuccess (void)
+{
+    emit success (this);
+}
+
+void medRunnableProcess::onFailure (void)
+{
+    emit failure (this);
+}
+
+void medRunnableProcess::onProgressed (int value)
+{
+    emit progressed (this, value);
+}
---
+++ src/medCore/medRunnableProcess.h
@@ -0,0 +1,51 @@
+/* medRunnableProcess.h --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008 - Julien Wintz, Inria.
+ * Created: Tue Dec 15 09:40:14 2009 (+0100)
+ * Version: $Id$
+ * Last-Updated: Tue Dec 15 09:40:15 2009 (+0100)
+ *           By: Julien Wintz
+ *     Update #: 1
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
+
+#ifndef MEDRUNNABLEPROCESS_H
+#define MEDRUNNABLEPROCESS_H
+
+#include "medJobItem.h"
+
+#include "medCoreExport.h"
+
+class medRunnableProcessPrivate;
+class dtkAbstractProcess;
+
+class MEDCORE_EXPORT medRunnableProcess : public medJobItem
+{
+    Q_OBJECT
+
+public:
+     medRunnableProcess(void);
+    ~medRunnableProcess(void);
+
+    void run(void);
+
+    void setProcess (dtkAbstractProcess *proc);
+
+public slots:
+    void onSuccess    (void);
+    void onFailure    (void);
+    void onProgressed (int);
+    
+private:
+    medRunnableProcessPrivate *d;
+};
+
+#endif // MEDRUNNABLEPROCESS_H
