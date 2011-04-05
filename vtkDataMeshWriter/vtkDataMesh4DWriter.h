4942d700554299fec129210e311692662ac3f2c5
Nicolas Toussaint <nicolas.toussaint@sophia.inria.fr>
2011-04-05 16:15:38 +0100

    forgot the writer parts for mesh comments

--- vtkDataMeshWriter/vtkDataMesh4DWriter.h
+++ vtkDataMeshWriter/vtkDataMesh4DWriter.h
@@ -6,6 +6,25 @@
 #include "vtkDataMeshWriterPluginExport.h"
 class vtkDataManagerWriter;
 
+
+/**
+    \class vtkDataMesh4DWriter
+    \brief This class implements the writing process of a sequence
+    (series) of meshes in vtk format.
+
+    It make extensive use of the vtkDataManager class and API (in
+    vtkINRIA3D repository). The ouput file is home-made and xml-based,
+    it derives from the API described in vtkDataManagerReader class.
+
+    The default extension (and the only extension currently wrote) is
+    *.v4d. The xml-based file lists a series of vtk files where time
+    tags have to be populated (file paths are relative). 
+    
+    \see vtkDataMesh4D vtkDataManagerWriter vtkMetaDataSetSequence
+    \author Nicolas Toussaint
+*/
+
+
 class VTKDATAMESHWRITERPLUGIN_EXPORT vtkDataMesh4DWriter : public dtkAbstractDataWriter
 {
     Q_OBJECT
