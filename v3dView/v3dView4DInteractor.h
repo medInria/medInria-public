d0c4432c49216241ee252e53f2d705ea6d0287be
Nicolas Toussaint <nicolas.toussaint@sophia.inria.fr>
2011-04-05 17:10:07 +0100

    commenting and adding features to the 4D interactor : now handles all image voxel types

--- v3dView/v3dView4DInteractor.h
+++ v3dView/v3dView4DInteractor.h
@@ -9,7 +9,6 @@
 #define V3DVIEW4DINTERACTOR_H
 
 #include <medCore/med4DAbstractViewInteractor.h>
-
 #include "v3dViewPluginExport.h"
 
 class v3dView4DInteractorPrivate;
@@ -17,8 +16,24 @@ class v3dView4DInteractorPrivate;
 class dtkAbstractData;
 class dtkAbstractView;
 
-class vtkCollection;
-class vtkMetaDataSetSequence;
+/**
+    \class v3dView4DInteractor
+    \brief This important interactor handles the 4th dimension
+    of data interacting with the viewing process.
+
+    \see med4DAbstractViewInteractor medViewerToolBoxTime 
+    \author Nicolas Toussaint and Fatih Arslan
+*/
+
+
+//BTX
+#define AppendImageSequenceMacro(type)				     \
+  vtkMetaDataSetSequence* sequence = vtkMetaDataSetSequence::New();  \
+  sequence->SetITKDataSet<type> (image);			     \
+  d->sequenceList->AddItem (sequence);				     \
+  sequence->Delete();						     \
+  this->updatePipeline();
+//ETX
 
 
 class V3DVIEWPLUGIN_EXPORT v3dView4DInteractor: public med4DAbstractViewInteractor
@@ -41,15 +56,15 @@ public:
     virtual void enable(void);
     virtual void disable(void);
 
-    //! Override dtkAbstractViewInteractor.
+    /** Override dtkAbstractViewInteractor method */
     virtual bool isAutoEnabledWith ( dtkAbstractData * data );
 
     void setCurrentTime (double time);
     double getCurrentTime (void)
     { return this->currentTime; }
-    //Assigns ranfe array the minimum and maximum time of the sequence
+    /** Assigns ranfe array the minimum and maximum time of the sequence */
     void sequencesRange (double* range);
-	//Returns minimum time step for the sequence 
+    /** Returns minimum time step for the sequence */
     double sequencesMinTimeStep (void);
 
 public slots:
