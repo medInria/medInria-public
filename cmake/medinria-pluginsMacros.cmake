76d03bc72d7daa0c7684c7464d2ada469cf7c12f
Stephan Schmitt <stephan.schmitt@inria.fr>
2011-05-19 11:36:01 +0200

    provide default values for each plugin indiviudually in CMakeLists.txt
    Conflicts:
    
    	CMakeLists.txt

--- cmake/medinria-pluginsMacros.cmake
+++ cmake/medinria-pluginsMacros.cmake
@@ -23,3 +23,32 @@ endif()
 
 
 ENDMACRO(MACRO_ADD_PLUGIN)
+
+
+# # argument LIST is a list with plugin names at odd indices followed by
+# # the respective default build values (ON/OFF) at even indices
+# MACRO(MACRO_ADD_PLUGIN_LIST P_LIST)
+  
+#   message("${P_LIST}")
+#   # list(LENGTH "${P_LIST}" N)
+#   list(LENGTH P_LIST N)
+#   message("${N}")
+#   while(N GREATER 0)
+#     list(GET P_LIST 0 PLUGIN_ITEM)
+#     list(GET P_LIST 1 BUILD_VALUE)
+#     if (NOT ${BUILD_VALUE} MATCHES "O(N|FF)")
+#       message ("The even members of the plugin list must be ON or OFF")
+#     endif()
+
+#     if (MEDINRIA_BUILD_ALL_PLUGINS)
+#       set(BUILD_VALUE ON)
+#     endif()
+
+#     MACRO_ADD_PLUGIN (${PLUGIN_ITEM} ${BUILD_VALUE})
+#     message(${PLUGIN_ITEM} " " ${BUILD_VALUE})
+
+#     list(REMOVE_AT P_LIST 0 1)
+#     list(LENGTH P_LIST N)
+#   endwhile()
+
+# ENDMACRO(MACRO_ADD_PLUGIN_LIST)
