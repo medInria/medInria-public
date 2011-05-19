1713c7709feaa79bff72b324ae3263b0ae108289
Benoît Bleuzé <benoit.bleuze@inria.fr>
2011-05-19 16:46:05 +0200

    does not empty the list of plugins in CMakelists.txt anymore

--- cmake/medinria-pluginsMacros.cmake
+++ cmake/medinria-pluginsMacros.cmake
@@ -13,11 +13,13 @@ string(TOUPPER ${P_NAME} UPPER_CASE_NAME)
 option(MEDINRIA_BUILD_${UPPER_CASE_NAME}_PLUGIN "Build ${P_NAME} plugin" "${DEFAULT_BUILD_VALUE}")
 
 if (MEDINRIA_BUILD_ALL_PLUGINS)
-    set(MEDINRIA_BUILD_${UPPER_CASE_NAME}_PLUGIN "ON")
+# Force to update the cache
+    set(MEDINRIA_BUILD_${UPPER_CASE_NAME}_PLUGIN "ON" CACHE BOOL "Build ${P_NAME} plugin" FORCE)
 endif()
 
 if (MEDINRIA_BUILD_ALL_PLUGINS OR
     MEDINRIA_BUILD_${UPPER_CASE_NAME}_PLUGIN )
+  #message("adding "${P_NAME})
   add_subdirectory(${P_NAME})
 endif()
 
@@ -27,28 +29,25 @@ ENDMACRO(MACRO_ADD_PLUGIN)
 
 # # argument LIST is a list with plugin names at odd indices followed by
 # # the respective default build values (ON/OFF) at even indices
-# MACRO(MACRO_ADD_PLUGIN_LIST P_LIST)
-  
-#   message("${P_LIST}")
-#   # list(LENGTH "${P_LIST}" N)
-#   list(LENGTH P_LIST N)
-#   message("${N}")
-#   while(N GREATER 0)
-#     list(GET P_LIST 0 PLUGIN_ITEM)
-#     list(GET P_LIST 1 BUILD_VALUE)
-#     if (NOT ${BUILD_VALUE} MATCHES "O(N|FF)")
-#       message ("The even members of the plugin list must be ON or OFF")
-#     endif()
+MACRO(MACRO_ADD_PLUGIN_LIST P_LIST)
+message("plugin list: " ${P_LIST})  
+list(LENGTH P_LIST N)
+message("list length:" ${N})
+set (INDEX 0)
+#message(${PLUGIN_LIST})
+while(INDEX LESS N)
+  MATH( EXPR INDEX_BOOL "${INDEX} + 1" )
+  MESSAGE ("index:" ${INDEX})
+  MESSAGE ("index_bool:" ${INDEX_BOOL})
 
-#     if (MEDINRIA_BUILD_ALL_PLUGINS)
-#       set(BUILD_VALUE ON)
-#     endif()
+  list(GET P_LIST ${INDEX} PLUGIN_ITEM)
+  list(GET P_LIST ${INDEX_BOOL} BUILD_VALUE)
 
-#     MACRO_ADD_PLUGIN (${PLUGIN_ITEM} ${BUILD_VALUE})
-#     message(${PLUGIN_ITEM} " " ${BUILD_VALUE})
+  message("Item: " ${PLUGIN_ITEM} "; value: " ${BUILD_VALUE})
 
-#     list(REMOVE_AT P_LIST 0 1)
-#     list(LENGTH P_LIST N)
-#   endwhile()
+  MACRO_ADD_PLUGIN (${PLUGIN_ITEM} ${BUILD_VALUE})
+  MATH( EXPR INDEX "${INDEX} + 2" )
+endwhile()
 
-# ENDMACRO(MACRO_ADD_PLUGIN_LIST)
+
+ENDMACRO(MACRO_ADD_PLUGIN_LIST)
