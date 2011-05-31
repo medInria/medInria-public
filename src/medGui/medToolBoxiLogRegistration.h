c4d92e0d1db52a0c8502428f1bc00cada6f834c9
Fatih Arslan <fatih.arslan@inria.fr>
2011-05-31 11:08:08 +0200

    Toolbox created

---
+++ src/medGui/medToolBoxiLogRegistration.h
@@ -0,0 +1,33 @@
+#ifndef MEDTOOLBOXILOGREGISTRATION_H
+#define MEDTOOLBOXILOGREGISTRATION_H
+
+#include <medGui/medToolBox.h>
+
+class dtkAbstractView;
+class dtkAbstractData;
+class medToolBoxiLogRegistrationPrivate;
+
+class MEDGUI_EXPORT medToolBoxiLogRegistration : public medToolBox
+{
+    Q_OBJECT
+
+public:
+     medToolBoxiLogRegistration(QWidget *parent = 0);
+    ~medToolBoxiLogRegistration(void);
+
+    
+	
+public slots:
+
+
+    
+ protected:
+
+    
+    
+ private:
+    medToolBoxiLogRegistrationPrivate *d;
+    
+};
+
+#endif MEDTOOLBOXILOGREGISTRATION_H
