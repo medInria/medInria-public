120226bbc79e32c78cf2962e6639cff2fe5484c4
Fatih Arslan <fatih.arslan@inria.fr>
2011-05-31 11:10:20 +0200

    New conf created

--- app/medinria/medViewerConfigurationSequenceRegistration.cpp
+++ app/medinria/medViewerConfigurationSequenceRegistration.cpp
@@ -1,7 +1,7 @@
 
 
 #include "medViewerConfigurationSequenceRegistration.h"
-
+#include <medGui/medToolBoxiLogRegistration.h>
 
 #include <medGui/medViewContainer.h>
 #include <medGui/medStackedViewContainers.h>
@@ -10,18 +10,19 @@
 class medViewerConfigurationSequenceRegistrationPrivate
 {
 public:
-    
+    medToolBoxiLogRegistration   *logToolBox;
 
 };
 
 medViewerConfigurationSequenceRegistration::medViewerConfigurationSequenceRegistration(QWidget *parent) : medViewerConfiguration(parent), d(new medViewerConfigurationSequenceRegistrationPrivate)
 {
     
-    showLayoutToolBox();
+    setLayoutToolBoxVisibility(false);
 
-   
+    d->logToolBox = new medToolBoxiLogRegistration(parent);
+    this->addToolBox( d->logToolBox );
     
-    
+    this->setLayoutType(medViewerConfiguration::LeftDbRightTb);
 }
 
 void medViewerConfigurationSequenceRegistration::setupViewContainerStack()
