369bdee65e7634a18a3daddcf21c5838a4c1a19b
Jaime Garcia Guevara <jaime.garcia_guevara@inria.fr>
2011-07-26 16:43:06 +0200

    change the order of this->onViewFocused(view) and put it after view->setSharedDataPointer(data) so the update is called after data is open fixes# 74

--- app/medinria/medViewerArea.cpp
+++ app/medinria/medViewerArea.cpp
@@ -241,10 +241,10 @@ void medViewerArea::open(const medDataIndex& index)
         // another hash?!
         medViewManager::instance()->insert(index, view);
         
-        this->onViewFocused(view);
-
         // set the data to the view
         view->setSharedDataPointer(data);
+
+        this->onViewFocused(view);
 
         // call update
         QMutexLocker ( &d->mutex );
