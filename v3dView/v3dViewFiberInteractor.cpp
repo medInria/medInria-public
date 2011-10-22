da603468e5821606483c7bd5be35809f1887691f
Pierre Fillard <pierre.fillard@inria.fr>
2011-10-22 13:53:57 +0200

    Activate bundling box only in 3D - send error message otherwise

--- v3dView/v3dViewFiberInteractor.cpp
+++ v3dView/v3dViewFiberInteractor.cpp
@@ -5,6 +5,8 @@
 #include <dtkCore/dtkAbstractView.h>
 #include <dtkCore/dtkAbstractViewFactory.h>
 
+#include <medMessageController.h>
+
 #include <vtkSmartPointer.h>
 #include <vtkPolyData.h>
 #include <vtkFiberDataSetManager.h>
@@ -229,6 +231,14 @@ void v3dViewFiberInteractor::onVisibilityPropertySet (const QString& value)
 
 void v3dViewFiberInteractor::onBoxVisibilityPropertySet (const QString& value)
 {
+    if (d->view && d->view->property("Orientation")!="3D") {
+        medMessageController::instance()->showError(this,
+                                                    "View must be in 3D mode to activate the bundling box",
+                                                    3000);
+        d->manager->SetBoxWidget(0);
+        return;
+    }
+
     if (value=="true")
         d->manager->SetBoxWidget(1);
     else
