c98a2dca01bc3902cf8cd6a6032d04accfb4604c
Benoît Bleuzé <benoit.bleuze@inria.fr>
2011-12-08 17:09:19 +0100

    change paths for user 'compiler'

--- utils/compil_tools/nightly/f32-med.sh
+++ utils/compil_tools/nightly/f32-med.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-./installer.py  -f run_linux.cfg -f run_fed.cfg -f run_f32-med.cfg -f run_nightly_linux.cfg --doc
+./installer.py  -f run_linux.cfg -f run_fed.cfg -f run_f32-med.cfg -f run_nightly_linux.cfg -f compiler.cfg --doc
