44342edb7e53eeeec6db44bd5a5ac7162ffe4aa8
Benoît Bleuzé <benoit.bleuze@inria.fr>
2011-12-20 16:44:38 +0100

    removed the --doc option for VMs other than f64

--- utils/compil_tools/nightly/u32-med.sh
+++ utils/compil_tools/nightly/u32-med.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
-./installer.py  -f run_linux.cfg -f run_ubuntu.cfg -f run_u32-med.cfg -f run_nightly_linux.cfg -f compiler.cfg --doc
+# no need for doc, f64 does it
+./installer.py  -f run_linux.cfg -f run_ubuntu.cfg -f run_u32-med.cfg -f run_nightly_linux.cfg -f compiler.cfg 
 
 ./installer.py -f run_linux.cfg -f run_ubuntu.cfg -f run_u32-med.cfg -f run_nightly_linux.cfg -f compiler.cfg -f debug.cfg --no-update-dirs --no-package --no-package-deps &> /dev/null
