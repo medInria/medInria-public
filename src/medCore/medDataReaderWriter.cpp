690cd221ced6a3821a56ab0bfd399eb4c5a14797
Benoît Bleuzé <benoit.bleuze@inria.fr>
2012-02-27 14:42:03 +0100

    Removed namespace until API discussion, renamed medDataReaderWriter

--- src/medCore/medDataReaderWriter.cpp
+++ src/medCore/medDataReaderWriter.cpp
@@ -2,76 +2,73 @@
 #include <dtkCore/dtkAbstractDataFactory.h>
 #include <QFileInfo>
 
-namespace medInria {
+medDataReaderWriter::Reader medDataReaderWriter::reader(const QString& path) {
+    QList<QString> readers = dtkAbstractDataFactory::instance()->readers();
 
-    DataReaderWriter::Reader DataReaderWriter::reader(const QString& path) {
-        QList<QString> readers = dtkAbstractDataFactory::instance()->readers();
-            
-        if (readers.size()==0) {
+    if (readers.size()==0) {
 #if 0
-            emit showError(this,tr("No reader plugin"),5000);
-            emit failure(this);
+        emit showError(this,tr("No reader plugin"),5000);
+        emit failure(this);
 #endif
-            return Reader();
-        }
+        return Reader();
+    }
 
-        const QFileInfo& fi(path);
-        const QString&   filename = fi.filePath();
+    const QFileInfo& fi(path);
+    const QString&   filename = fi.filePath();
 
-        //  Store the last reader.
+    //  Store the last reader.
 
-        static Reader dreader;
+    static Reader dreader;
 
-        if (!dreader.isNull() && dreader->canRead(filename))
+    if (!dreader.isNull() && dreader->canRead(filename))
+        return dreader;
+
+    for (int i=0;i<readers.size();++i) {
+        dreader = dtkAbstractDataFactory::instance()->readerSmartPointer(readers[i]);
+        if (dreader->canRead(filename)) {
+            dreader->enableDeferredDeletion(false);
             return dreader;
-
-        for (int i=0;i<readers.size();++i) {
-            dreader = dtkAbstractDataFactory::instance()->readerSmartPointer(readers[i]);
-            if (dreader->canRead(filename)) {
-                dreader->enableDeferredDeletion(false);
-                return dreader;
-            }
         }
-        return dreader = Reader();
     }
+    return dreader = Reader();
+}
 
-    DataReaderWriter::Writer DataReaderWriter::writer(const QString& path,const dtkAbstractData* data) {
+medDataReaderWriter::Writer medDataReaderWriter::writer(const QString& path,const dtkAbstractData* data) {
 
-        if (!data)
-            return Writer();
+    if (!data)
+        return Writer();
 
-        QList<QString> writers = dtkAbstractDataFactory::instance()->writers();
+    QList<QString> writers = dtkAbstractDataFactory::instance()->writers();
 
-        static Writer dwriter;
+    static Writer dwriter;
 
-        if (!dwriter.isNull() && dwriter->handled().contains(data->identifier()) && dwriter->canWrite(path))
+    if (!dwriter.isNull() && dwriter->handled().contains(data->identifier()) && dwriter->canWrite(path))
+        return dwriter;
+
+    for (int i=0;i<writers.size();++i) {
+        dwriter = dtkAbstractDataFactory::instance()->writerSmartPointer(writers[i]);
+        if (dwriter->handled().contains(data->identifier()) && dwriter->canWrite(path)) {
+            dwriter->enableDeferredDeletion(false);
             return dwriter;
-
-        for (int i=0;i<writers.size();++i) {
-            dwriter = dtkAbstractDataFactory::instance()->writerSmartPointer(writers[i]);
-            if (dwriter->handled().contains(data->identifier()) && dwriter->canWrite(path)) {
-                dwriter->enableDeferredDeletion(false);
-                return dwriter;
-            }
-        }
-        return dwriter = Writer();
-    }
-
-    DataReaderWriter::Data DataReaderWriter::read(const QString& path) {
-        Reader dreader = reader(path);
-        if (!dreader.isNull()) {
-            dreader->read(path);
-            return dreader->data();
         }
     }
+    return dwriter = Writer();
+}
 
-    bool DataReaderWriter::write(const QString& path,dtkAbstractData* data) {
-        Writer dwriter = writer(path,data);
-        if (!dwriter.isNull()) {
-            dwriter->setData(data);
-            dwriter->write(path);
-            return true;
-        }
-        return false;
-    }  
+medDataReaderWriter::Data medDataReaderWriter::read(const QString& path) {
+    Reader dreader = reader(path);
+    if (!dreader.isNull()) {
+        dreader->read(path);
+        return dreader->data();
+    }
+}
+
+bool medDataReaderWriter::write(const QString& path,dtkAbstractData* data) {
+    Writer dwriter = writer(path,data);
+    if (!dwriter.isNull()) {
+        dwriter->setData(data);
+        dwriter->write(path);
+        return true;
+    }
+    return false;
 }
--- src/medCore/medDataReaderWriter.h
+++ src/medCore/medDataReaderWriter.h
@@ -6,20 +6,17 @@
 #include <dtkCore/dtkAbstractDataWriter.h>
 #include <dtkCore/dtkAbstractData.h>
 
-namespace medInria {
+struct medDataReaderWriter {
 
-    struct DataReaderWriter {
+    typedef dtkSmartPointer<dtkAbstractDataReader> Reader;
+    typedef dtkSmartPointer<dtkAbstractDataWriter> Writer;
+    typedef dtkSmartPointer<dtkAbstractData>       Data;
 
-        typedef dtkSmartPointer<dtkAbstractDataReader> Reader;
-        typedef dtkSmartPointer<dtkAbstractDataWriter> Writer;
-        typedef dtkSmartPointer<dtkAbstractData>       Data;
+    static Reader reader(const QString& path);
+    static Writer writer(const QString& path,const dtkAbstractData* data);
 
-        static Reader reader(const QString& path);
-        static Writer writer(const QString& path,const dtkAbstractData* data);
-
-        static Data read(const QString& path);
-        static bool write(const QString& path,dtkAbstractData* data);
+    static Data read(const QString& path);
+    static bool write(const QString& path,dtkAbstractData* data);
     };
-}
 
 #endif  //  ! MED_DATA_READER_WRITER_H
