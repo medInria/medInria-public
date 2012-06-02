8e6761df255b5fa71de8c9f1010db4f66cbc8dc5
Pierre Fillard <pierre.fillard@inria.fr>
2012-06-02 14:38:47 +0200

    Adding medTest lib with test helper functions + first tests (medGui/viewContainers)

---
+++ src/medTest/medQtDataImage.h
@@ -0,0 +1,72 @@
+/*
+ * medQtDataImage.h
+ *
+ *  Created on: 4 juil. 2011 10:29:14
+ *      Author: John Stark
+ */
+
+#ifndef MEDQTDATAIMAGE_H_
+#define MEDQTDATAIMAGE_H_
+
+#include "medTestExport.h"
+
+#include <medCore/medAbstractDataImage.h>
+
+class medQtDataImagePrivate;
+
+/** A concrete implementation of a dtkAbstractDataImage for use in testing. */
+class MEDTEST_EXPORT medQtDataImage : public medAbstractDataImage
+{
+    Q_OBJECT;
+public:
+    medQtDataImage();
+    virtual ~medQtDataImage();
+    virtual QString description(void) const;
+    virtual QString identifier(void) const;
+
+    static QString s_description();
+    static QString s_identifier();
+    static bool registered();
+
+    // Implement dtkAbstractData
+    virtual void setData(void* data);
+    virtual void setData(void* data, int channel);
+    virtual void *output(void);
+    virtual void *data(void);
+    virtual void *data(int channel);
+
+    virtual       QImage & thumbnail(void)  const;
+    virtual QList<QImage>& thumbnails(void) const;
+
+    // Implement dtkAbstractDataImage
+    void *image(void);
+
+    virtual int   Dimension(void) const;
+    virtual const PixId& PixelType(void) const;
+
+    virtual int xDimension(void) const;
+    virtual int yDimension(void) const;
+    virtual int zDimension(void) const;
+    virtual int tDimension(void) const;
+
+    virtual qreal xSpacing(void) const;
+    virtual qreal ySpacing(void) const;
+    virtual qreal zSpacing(void) const;
+    virtual qreal tSpacing(void) const;
+
+    virtual int minRangeValue(void);
+    virtual int maxRangeValue(void);
+
+    virtual int scalarValueCount(int value);
+    virtual int scalarValueMinCount(void);
+    virtual int scalarValueMaxCount(void);
+public:
+protected:
+    void generateThumbnails() const;
+private:
+
+    medQtDataImagePrivate * d;
+};
+
+#endif /* MEDQTDATAIMAGE_H_ */
+
---
+++ src/medTest/medTest.h
@@ -0,0 +1,46 @@
+#ifndef MEDTEST_H
+#define MEDTEST_H
+
+#include "medTestExport.h"
+
+#include <dtkCore/dtkTest.h>
+#include <dtkCore/dtkSmartPointer.h>
+#include <dtkCore/dtkAbstractData.h>
+
+namespace medTest
+{
+    /**
+     * Get the string contained in the environment
+     * variable var. Returns false if the variable is empty or
+     * not defined.
+     */
+    MEDTEST_EXPORT bool envString (const QString &var, QString &value);
+
+    /**
+     * Initialize the medPluginManager with paths defined
+     * in MEDINRIA_PLUGINS_PATH. Test will fail is environment
+     * variable is not defined or empty.
+     */
+    MEDTEST_EXPORT void initializePlugins(void);
+
+    /**
+     * Clean-up medPluginManager.
+     */
+    MEDTEST_EXPORT void uninitializePlugins(void);
+
+    /**
+     * Initialize Qt resource system with resource file defined
+     * in environment variable var.
+     */
+    MEDTEST_EXPORT void initializeResources(const QString &var);
+
+    /**
+     * Returns a dtkAbstractData instance by reading file.
+     * Make sure that a db controller has been registered prior to
+     * using this function.
+     */
+    MEDTEST_EXPORT dtkSmartPointer<dtkAbstractData> readData (const QString &file);
+
+} // end of namespace
+
+#endif
