a5f6253520f225e32f38ab3cde6430438a8a49de
Olivier Commowick <olivier.commowick@inria.fr>
2013-02-02 13:19:33 +0100

    TR1 does not exist on macos either

--- vtkVisuManagement/vtkSphericalHarmonicSource.cxx
+++ vtkVisuManagement/vtkSphericalHarmonicSource.cxx
@@ -28,13 +28,13 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Compute spherical associated Legendre function
-#ifdef WIN32
+#if (defined __APPLE__ || defined WIN32)
 #include <boost/math/special_functions/legendre.hpp>
 #else
 #include <tr1/cmath>
 #endif //WIN32
 
-#ifdef WIN32
+#if (defined __APPLE__ || defined WIN32)
 double sphLegendre(int _l, int _m, double theta) {
   double factor = sqrt(((double)(2*_l+1) / (4.0*vtkMath::DoublePi()))*(boost::math::factorial<double>((unsigned int)(_l - _m))
                                                         / boost::math::factorial<double>((unsigned int)(_l + _m))))*boost::math::legendre_p (_l, _m, cos(theta));
