4334934eb27fb62d586e5a9b6267348e5c5ca2c2
René-paul Debroize <rene-paul.debroize@inria.fr>
2013-02-08 15:42:58 +0100

    add missing file : CMakeIncludeDirectories.cmake

---
+++ CMakeIncludeDirectories.cmake
@@ -0,0 +1,11 @@
+include_directories(
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/vtkDataManagement
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/vtkItk
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/vtkVisuManagement        
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/vtkWidgetsAddOn
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/vtkImageView    
+        ${CMAKE_CURRENT_SOURCE_DIR}/libs/vtkInria/itkAddOn/IO/
+        )
+
