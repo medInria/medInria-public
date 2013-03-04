91cfc0d0e840ca88d1d50706615627d3cba59a57
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-03-04 17:23:39 +0100

    This commit allow to medinria-plugins directly from medinria and clean one more time vtkInria

--- libs/vtkInria/CTestConfig.cmake
+++ libs/vtkInria/CTestConfig.cmake
@@ -1,7 +1,7 @@
-set(CTEST_PROJECT_NAME "vtkINRIA3D")
+set(CTEST_PROJECT_NAME "vtkInria")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.inria.fr")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=vtkINRIA3D")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=vtkInria")
 set(CTEST_DROP_SITE_CDASH TRUE)
