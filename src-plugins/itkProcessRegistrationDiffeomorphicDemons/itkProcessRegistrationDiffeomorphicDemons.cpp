620b3c8f2d79fbeebcc4e947b00b069aba064529
Hakim Fadil <moulay.fadil@inria.fr>
2013-03-06 15:58:20 +0100

    a minor change

--- src-plugins/itkProcessRegistrationDiffeomorphicDemons/itkProcessRegistrationDiffeomorphicDemons.cpp
+++ src-plugins/itkProcessRegistrationDiffeomorphicDemons/itkProcessRegistrationDiffeomorphicDemons.cpp
@@ -30,7 +30,7 @@
 
 #include <DiffeomorphicDemons/rpiDiffeomorphicDemons.hxx>
 #include <rpiCommonTools.hxx>
-#include <hRegistrationFactory\hRegistrationFactory.h>
+#include <hRegistrationFactory/hRegistrationFactory.h>
 
 
 // /////////////////////////////////////////////////////////////////
