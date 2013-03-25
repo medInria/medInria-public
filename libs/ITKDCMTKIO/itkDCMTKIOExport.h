525453051db1478cb4242496fe5474f80153ffd5
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-03-25 13:32:53 +0100

    this commit fix itkDCMTKIOExport.h

--- libs/ITKDCMTKIO/itkDCMTKIOExport.h
+++ libs/ITKDCMTKIO/itkDCMTKIOExport.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(WIN32) && defined(ITK_BUILD_SHARED)
+#if defined(WIN32)
     #ifdef ITKDCMTKIO_EXPORTS
         #define ITKDCMTKIO_EXPORT __declspec(dllexport) 
     #else
