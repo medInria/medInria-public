9042454b0dfde5b1ad42b1b44e04ab078efb0f9f
Olivier Commowick <olivier.commowick@inria.fr>
2013-04-11 10:13:56 +0200

    Remove some useless files, move includes of medinria-plugins to medinria

--- cmake/medInriaBuild.cmake
+++ cmake/medInriaBuild.cmake
@@ -38,7 +38,22 @@ set(${PROJECT_NAME}_INCLUDE_DIR
     ${PROJECT_SOURCE_DIR}/src/medGui/settingsWidgets
     ${PROJECT_SOURCE_DIR}/src/medGui/toolboxes
     ${PROJECT_SOURCE_DIR}/src/medGui/viewContainers
-	${PROJECT_SOURCE_DIR}/src/medGui/database)
+    ${PROJECT_SOURCE_DIR}/src/medGui/database
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/ITKDCMTKIO
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/dtkVtkLogForwarder
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/dtItkLogForwarder
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/itkDataImageBase
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/itkProcessRegistration    
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/medPluginsPch                    
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/vtkDataManagement
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/vtkItk
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/vtkVisuManagement        
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/vtkWidgetsAddOn
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/vtkImageView    
+    ${PROJECT_SOURCE_DIR}/src-plugins/libs/vtkInria/itkAddOn/IO/
+)
 
 include_directories(${${PROJECT_NAME}_INCLUDE_DIR})
 
@@ -55,7 +70,22 @@ set(${PROJECT_NAME}_INSTALL_INCLUDE_DIR
     ${CMAKE_INSTALL_PREFIX}/include/medGui/settingsWidgets
     ${CMAKE_INSTALL_PREFIX}/include/medGui/toolboxes
     ${CMAKE_INSTALL_PREFIX}/include/medGui/viewContainers
-	${CMAKE_INSTALL_PREFIX}/include/medGui/database)
+    ${CMAKE_INSTALL_PREFIX}/include/medGui/database
+    ${CMAKE_INSTALL_PREFIX}/include/libs
+    ${CMAKE_INSTALL_PREFIX}/include/libs/ITKDCMTKIO
+    ${CMAKE_INSTALL_PREFIX}/include/libs/dtkVtkLogForwarder
+    ${CMAKE_INSTALL_PREFIX}/include/libs/dtItkLogForwarder
+    ${CMAKE_INSTALL_PREFIX}/include/libs/itkDataImageBase
+    ${CMAKE_INSTALL_PREFIX}/include/libs/itkProcessRegistration    
+    ${CMAKE_INSTALL_PREFIX}/include/libs/medPluginsPch                    
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/vtkDataManagement
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/vtkItk
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/vtkVisuManagement        
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/vtkWidgetsAddOn
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/vtkImageView    
+    ${CMAKE_INSTALL_PREFIX}/include/libs/vtkInria/itkAddOn/IO/
+)
 
 
 ## ###################################################################
