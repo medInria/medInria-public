e9e0219c13f742c1301642d656ecfe6fa229215e
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-04-16 14:15:33 +0200

    Revert "remove SCripts and vtkItk from vtkInria"
    This reverts commit e4ab369decb315780c6507309a9a22b843017925.

---
+++ src-plugins/libs/vtkInria/Scripts/continuouswin.bat
@@ -0,0 +1,2 @@
+call "C:/Program Files/Microsoft Visual Studio .NET 2003/Vc7/bin/vcvars32.bat"
+call "C:/Program Files/CMake 2.4/bin/ctest.exe" -S D:\Work\vtkINRIA3D\src\Scripts\continuous_win.cmake -V
---
+++ src-plugins/libs/vtkInria/Scripts/experimental.cmake
@@ -0,0 +1,32 @@
+SET (CTEST_SOURCE_DIRECTORY "/u/sylvius/home/dashboardBuilds/vtkINRIA3D/experimental/src")
+SET (CTEST_BINARY_DIRECTORY "/u/sylvius/home/dashboardBuilds/vtkINRIA3D/experimental/linux-build")
+
+SET (CTEST_CVS_COMMAND   "svn")
+SET (CTEST_CVS_CHECKOUT  "${CTEST_CVS_COMMAND} co svn://scm.gforge.inria.fr/svn/vtkinria3d ${CTEST_SOURCE_DIRECTORY}")
+
+# which ctest command to use for running the dashboard
+SET (CTEST_COMMAND "/proj/asclepios/home/import/CMake/testing/install/linux_FC3/bin/ctest -D Experimental")
+
+# what cmake command to use for configuring this dashboard
+SET (CTEST_CMAKE_COMMAND "/proj/asclepios/home/import/CMake/testing/install/linux_FC3/bin/cmake")
+
+
+####################################################################
+# The values in this section are optional you can either
+# have them or leave them commented out
+####################################################################
+
+# should ctest wipe the binary tree before running
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+SET (CTEST_INITIAL_CACHE "
+VTK_DIR:STRING=/proj/asclepios/home/import/VTK/VTK/release/install/linux_FC3/lib/vtk-5.0
+vtkINRIA3D_USE_ITK:BOOL=ON
+ITK_DIR:STRING=/proj/asclepios/home/import/ITK/Insight/release/install/linux_FC3/lib/InsightToolkit
+vtkINRIA3D_USE_KWWIDGETS:BOOL=ON
+vtkINRIA3D_USE_WXWINDOWS:BOOL=ON
+wxWidgets_CONFIG_EXECUTABLE:STRING=/proj/asclepios/home/import/wxWidgets/release/install/linux_FC3/bin/wx-config
+KWWidgets_DIR:STRING=/proj/asclepios/home/import/KWWidgets/install/linux_FC3/lib/KWWidgets
+")
