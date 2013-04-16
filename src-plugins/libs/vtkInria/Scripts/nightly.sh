73bdfc1baee999cb7c0adac937dc6c0ee13021d7
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-04-16 14:17:20 +0200

    remove SCripts from vtkInria

--- src-plugins/libs/vtkInria/Scripts/nightly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PKG_CONFIG_PATH=/proj/asclepios/home/import/gtkglext/lib/pkgconfig:$PKG_CONFIG_PATH
-
-export SCRIPT_DIR=/home/dashboardBuilds/vtkINRIA3D/src/Scripts
-cd $SCRIPT_DIR
-/proj/asclepios/home/import/CMake/testing/install/linux_FC3/bin/ctest -S nightly.cmake
--- src-plugins/libs/vtkInria/Scripts/nightlylwin.bat
+++
@@ -1,2 +0,0 @@
-call "C:/Program Files/Microsoft Visual Studio .NET 2003/Vc7/bin/vcvars32.bat"
-call "D:/CMake 2.4/bin/ctest.exe" -S D:\pfillard\vtkINRIA3D\nightly_win.cmake -V
