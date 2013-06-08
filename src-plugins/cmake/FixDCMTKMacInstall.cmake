c6e25dd912c6759b3beac7774d1e3b431647a1ba
Olivier Commowick <olivier.commowick@inria.fr>
2013-06-08 15:43:48 +0200

    DCMTK links need fix on mac only if not built static

--- src-plugins/cmake/FixDCMTKMacInstall.cmake
+++ src-plugins/cmake/FixDCMTKMacInstall.cmake
@@ -14,9 +14,13 @@
 MACRO(FixDCMTKMacInstall)
 
   foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
-    foreach(linkedlib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
-      execute_process(COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${linkedlib}.dylib ${DCMTK_DIR}/lib/lib${linkedlib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib)
-    endforeach()
+    if (EXISTS ${DCMTK_DIR}/lib/lib${lib}.dylib)
+      foreach(linkedlib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
+        if (EXISTS ${DCMTK_DIR}/lib/lib${linkedlib}.dylib)
+          execute_process(COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${linkedlib}.dylib ${DCMTK_DIR}/lib/lib${linkedlib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib)
+        endif()
+      endforeach()
+    endif()
   endforeach()
 
 ENDMACRO(FixDCMTKMacInstall)
