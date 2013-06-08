2885ec04871ae295bdfe10c212dd09413254befc
Olivier Commowick <olivier.commowick@inria.fr>
2013-06-08 15:43:48 +0200

    DCMTK links need fix on mac only if not built static

--- src-plugins/cmake/FixDCMTKMacLink.cmake
+++ src-plugins/cmake/FixDCMTKMacLink.cmake
@@ -14,11 +14,13 @@
 MACRO(FixDCMTKMacLink lib_name)
 
   foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
-    add_custom_command(TARGET ${lib_name}
-	POST_BUILD
-	COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/lib${lib_name}.dylib
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BIN_DIR}
-    )
+    if(EXISTS "${DCMTK_DIR}/lib/lib${lib}.dylib")
+      add_custom_command(TARGET ${lib_name}
+	    POST_BUILD
+	    COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/lib${lib_name}.dylib
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BIN_DIR}
+      )
+    endif()
   endforeach()
 
 ENDMACRO(FixDCMTKMacLink)
