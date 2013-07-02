3de7327fd5ba0d2ad4ef5e1e38abd74cf7dbc31c
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-07-02 16:46:58 +0200

    remove useless packaging stuff

--- utils/compil_tools/nightly/f64-med.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#only script building the doc
-./installer.py  -f run_linux.cfg -f run_fed.cfg -f run_f64-med.cfg -f run_nightly_linux.cfg -f compiler.cfg --doc 
-
-./installer.py -f run_linux.cfg -f run_fed.cfg -f run_f64-med.cfg -f run_nightly_linux.cfg -f compiler.cfg -f debug.cfg --no-update-dirs --no-package --no-package-deps &> /dev/null
