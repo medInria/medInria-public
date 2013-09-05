298201af18a17938a21fcc1f0af08b931ea2a183
Olivier Commowick <olivier.commowick@inria.fr>
2013-09-05 09:27:07 +0200

    fixes #405, uses itk::GradientFileReader from TTK to support gradient files with tabs

--- src-plugins/itkDataDiffusionGradientList/itkDataDiffusionGradientList.cpp
+++ src-plugins/itkDataDiffusionGradientList/itkDataDiffusionGradientList.cpp
@@ -15,6 +15,8 @@
 
 #include <dtkCore/dtkAbstractDataFactory.h>
 
+#include <itkGradientFileReader.h>
+
 #include <QFile>
 #include <QVector>
 #include <QString>
@@ -78,129 +80,27 @@ bool itkDataDiffusionGradientList::read (const QString& filename)
 {
     typedef itkDataDiffusionGradientListPrivate::GradientType     GradientType;
     typedef itkDataDiffusionGradientListPrivate::GradientListType GradientListType;
-    
-
-    QFile textfile( filename );
-    if (!textfile.open(QIODevice::ReadOnly | QIODevice::Text))
-        return false;
-
-    
-    QVector<QString> file;
-    QTextStream in( &textfile );
-    while ( !in.atEnd() ) {
-        QString line = in.readLine();
-        file.push_back ( line );
-    }
-
-
-    QVector<QString> aux;
-    for( int i=0; i<file.size(); i++) {
-        QString line = file[i].trimmed();
-        if( line.length()>0 && line[0]!='#' ) {
-            aux.push_back (line);
-        }
-    }
-    file = aux;
-
-
-    QVector< QVector<double> > numbers;
-    int total = 0;
-    for( int i=0; i<file.size(); i++) {
-
-        QStringList num_list = file[i].split (" ", QString::SkipEmptyParts);
-
-        QVector< double > num;
-        for( int j=0; j<num_list.size(); j++) {
-            bool ok = false;
-            double val = num_list[j].toDouble ( &ok );
-            if( !ok ) {
-                qDebug() << "Unexpected character when parsing line: " << file[i].toAscii().constData();
-                return false;
-            }
-            num.push_back ( val );
-            total++;
-        }
-        numbers.push_back ( num );
-    }
-
-
+        
     GradientListType output;
+    //Read Gradients
+    typedef itk::GradientFileReader ReaderType;
+    typedef ReaderType::VectorListType VectorListType;
+    typedef ReaderType::VectorType VectorType;
     
-    if( total%3==0 ) { // there are 3xsomething numbers
-        // 2 cases: either Nx3 or 3xN:
-        if ( numbers.size()==3 ) {
-            if( numbers[0].size()!=numbers[1].size() && numbers[0].size()!=numbers[2].size() ){
-                qDebug() << "Error: Lines have not the same length.";
-                return false;
-            }
-
-            for( int i=0; i<numbers[0].size(); i++) {
-                GradientType grad (3);
-                grad[0] = numbers[0][i];
-                grad[1] = numbers[1][i];
-                grad[2] = numbers[2][i];
-
-                output.push_back (grad);
-            }
-        }
-        else {
-            for( int i=0; i<numbers.size(); i++) {
-                if (numbers[i].size() != 3 ){
-                    qDebug() << "Error: There should be 3 gradients per line, check the file.";
-                    return false;
-                }
-
-                GradientType grad (3);
-                grad[0] = numbers[i][0];
-                grad[1] = numbers[i][1];
-                grad[2] = numbers[i][2];
-
-                output.push_back (grad);
-            }
-        }
-    }
-    else if ( (total-1)%3==0 ) {// we assume here that there is a number giving the total number of gradients to read
-        double numOfGrad = numbers[0][0];
-        if( numOfGrad != (double)(numbers.size())-1 ) {
-            char Message[512];
-            sprintf( Message, "Error: The number of gradients (%d) does not match the actual number of lines (%d).", (int)numOfGrad, (int)numbers.size()-1);
-            qDebug() << Message;
-            return false;
-        }
-        for( int i=1; i<numbers.size(); i++) {
-            if (numbers[i].size() != 3 ){
-                qDebug() << "Error: There should be 3 gradients per line, check the file.";
-                return false;
-            }
-
-            GradientType grad (3);
-            grad[0] = numbers[i][0];
-            grad[1] = numbers[i][1];
-            grad[2] = numbers[i][2];
-
-            output.push_back (grad);
-        }
-    }
-    else {
-        qDebug() << "Error: Unrecognized file format.";
-        return false;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName (filename.toStdString());
+    reader->Update();
+    VectorListType gradients = reader->GetGradientList();
+    
+    for(int i=0; i<(gradients.size()); i++)
+    {
+        GradientType grad (3);
+        grad[0] = gradients[i][0];
+        grad[1] = gradients[i][1];
+        grad[2] = gradients[i][2];
+        output.push_back (grad);
     }
     
-
-    /*
-    // now check if the first gradient is [0,0,0]
-    GradientType grad = output[0];
-    if( grad[0]==0.0 && grad[1]==0.0 && grad[2]==0.0 ){
-
-      // we remove the null gradient
-      GradientListType listG ( output.size() - 1 );
-      for( int i=1; i<output.size(); i++) {
-        listG[i-1] = output[i];
-      }
-      output = listG;
-    }
-   */
-
     this->setNumberOfChannels (output.size());
 
     d->gradients = output;
