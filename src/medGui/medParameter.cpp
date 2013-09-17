99279aaf5e6acfae3f5279d666c770f538fcb927
Guillaume Pasquier <guillaume.pasquier@inria.fr>
2013-09-17 17:05:56 +0200

    update in medParameter and medParameterPool creation

--- src/medGui/medParameter.cpp
+++ src/medGui/medParameter.cpp
@@ -2,13 +2,69 @@
 #include <QStringList>
 
 
+//------------------ medActionParameter -------------------------------------
+
+medActionParameter::medActionParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
+{
+     setupWidget();
+}
+
+void medActionParameter::setIcon(QIcon icon)
+{
+    _button->setIcon(icon);
+}
+
+void medActionParameter::setText(QString value)
+{
+    _button->setText(value);
+}
+
+void medActionParameter::setCheckable(bool value)
+{
+    _button->setCheckable(value);
+}
+
+
+void medActionParameter::setupWidget()
+{
+    initializeWidget();
+    _button = new QPushButton;
+    _button->setToolTip(_name);
+    _layout->addWidget(_button);
+
+    connect(_button, SIGNAL(clicked()), this, SLOT(emitTriggered()));
+    connect(_button, SIGNAL(toggled(bool)), this, SLOT(emitTriggered(bool)));
+}
+
+void medActionParameter::emitTriggered(bool value)
+{
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+    {
+      emit triggered(dynamic_cast<dtkAbstractData*>(_attachedObject));
+      emit triggered(dynamic_cast<dtkAbstractData*>(_attachedObject), value);
+    }
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+    {
+      emit triggered(dynamic_cast<dtkAbstractProcess*>(_attachedObject));
+      emit triggered(dynamic_cast<dtkAbstractProcess*>(_attachedObject), value);
+    }
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+    {
+      emit triggered(dynamic_cast<dtkAbstractView*>(_attachedObject));
+      emit triggered(dynamic_cast<dtkAbstractView*>(_attachedObject), value);
+    }
+
+    emit triggered();
+}
+
+
 //------------------ medBooleanParameter -------------------------------------
 
-medBooleanParameter::medBooleanParameter(QString name, dtkAbstractObject *attachedData)
-    : medAbstractParameter(name, attachedData)
+medBooleanParameter::medBooleanParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
 {
-    _attachedData = attachedData;
-        setupWidget();
+     setupWidget();
 }
 
 void medBooleanParameter::setValue(bool value)
@@ -23,6 +79,7 @@ bool medBooleanParameter::value()
 
 void medBooleanParameter::setupWidget()
 {
+    initializeWidget();
     _checkbox = new QCheckBox;
     _layout->addWidget(_checkbox);
 
@@ -31,23 +88,27 @@ void medBooleanParameter::setupWidget()
 
 void medBooleanParameter::emitValueChanged(bool value)
 {
-    if(dynamic_cast<dtkAbstractData*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedData) , value);
-    else if(dynamic_cast<dtkAbstractProcess*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedData) , value);
-    else emit valueChanged(value);
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+
+    emit valueChanged(value);
 }
 
 //------------------ medDoubleParameter -------------------------------------
 
-medDoubleParameter::medDoubleParameter(QString name, dtkAbstractObject *attachedData)
-    : medAbstractParameter(name, attachedData)
+medDoubleParameter::medDoubleParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
 {
         setupWidget();
 }
 
 void medDoubleParameter::setupWidget()
 {
+    initializeWidget();
     _slider = new medDoubleSliderSpinboxPair;
     _layout->addWidget(_slider);
     connect(_slider, SIGNAL(valueChanged(double)), this, SLOT(emitValueChanged(double)));
@@ -75,23 +136,27 @@ void medDoubleParameter::setMaximum(const double max)
 
 void medDoubleParameter::emitValueChanged(double value)
 {
-    if(dynamic_cast<dtkAbstractData*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedData) , value);
-    else if(dynamic_cast<dtkAbstractProcess*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedData) , value);
-    else emit valueChanged(value);
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+
+    emit valueChanged(value);
 }
 
 //------------------ medIntParameter -------------------------------------
 
-medIntParameter::medIntParameter(QString name, dtkAbstractObject *attachedData)
-    : medAbstractParameter(name, attachedData)
+medIntParameter::medIntParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
 {
         setupWidget();
 }
 
 void medIntParameter::setupWidget()
 {
+    initializeWidget();
     _slider = new medSliderSpinboxPair;
     _layout->addWidget(_slider);
     connect(_slider, SIGNAL(valueChanged(int)), this, SLOT(emitValueChanged(int)));
@@ -119,26 +184,31 @@ void medIntParameter::setMaximum(const int max)
 
 void medIntParameter::emitValueChanged(int value)
 {
-    if(dynamic_cast<dtkAbstractData*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedData) , value);
-    else if(dynamic_cast<dtkAbstractProcess*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedData) , value);
-    else emit valueChanged(value);
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+
+    emit valueChanged(value);
 }
 
 //------------------ medListParameter -------------------------------------
 
-medListParameter::medListParameter(QString name, dtkAbstractObject *attachedData)
-    : medAbstractParameter(name, attachedData)
+medListParameter::medListParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
 {
     setupWidget();
 }
 
 void medListParameter::setupWidget()
 {
+    initializeWidget();
     _comboBox = new QComboBox;
     _layout->addWidget(_comboBox);
     connect(_comboBox, SIGNAL(currentIndexChanged(QString)), this, SLOT(emitValueChanged(QString)));
+    connect(_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(emitValueChanged(int)));
 }
 
 void medListParameter::setValues(QStringList values)
@@ -159,9 +229,233 @@ QString medListParameter::value()
 
 void medListParameter::emitValueChanged(QString value)
 {
-    if(dynamic_cast<dtkAbstractData*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedData) , value);
-    else if(dynamic_cast<dtkAbstractProcess*>(_attachedData) )
-      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedData) , value);
-    else emit valueChanged(value);
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+
+    emit valueChanged(value);
+}
+
+void medListParameter::emitValueChanged(int value)
+{
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+      emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+
+    emit valueChanged(value);
+}
+
+//------------------ medColorListParameter -------------------------------------
+
+medColorListParameter::medColorListParameter(QString name, dtkAbstractObject *attachedObject)
+    : medAbstractParameter(name, attachedObject)
+{
+    setupWidget();
+}
+
+void medColorListParameter::setupWidget()
+{
+    initializeWidget();
+    _comboBox = new QComboBox;
+    _layout->addWidget(_comboBox);
+    connect(_comboBox, SIGNAL(currentIndexChanged(QString)), this, SLOT(emitValueChanged(QString)));
+}
+
+void medColorListParameter::setValues(QList<QColor> values)
+{
+    _comboBox->clear();
+
+    foreach(QColor color, values)
+    {
+        _comboBox->addItem(createIcon(color.name()),color.name(),color);
+    }
+}
+
+void medColorListParameter::setValues(QStringList values)
+{
+    _comboBox->clear();
+
+    foreach(QString color, values)
+    {
+        _comboBox->addItem(createIcon(color),color,QColor(color));
+    }
+}
+
+void medColorListParameter::setDefaultValue(QString value)
+{
+    _comboBox->setCurrentIndex(_comboBox->findText(value));
+}
+
+void medColorListParameter::setDefaultValue(QColor value)
+{
+    _comboBox->setCurrentIndex(_comboBox->findText(value.name()));
+}
+
+QString medColorListParameter::value()
+{
+    return _comboBox->currentText();
+}
+
+void medColorListParameter::emitValueChanged(QString value)
+{
+    QColor color = QColor(value);
+
+    if(dynamic_cast<dtkAbstractData*>(_attachedObject) )
+    {
+        emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , value);
+        emit valueChanged(dynamic_cast<dtkAbstractData*>(_attachedObject) , color);
+    }
+    else if(dynamic_cast<dtkAbstractProcess*>(_attachedObject) )
+    {
+        emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , value);
+        emit valueChanged(dynamic_cast<dtkAbstractProcess*>(_attachedObject) , color);
+    }
+    else if(dynamic_cast<dtkAbstractView*>(_attachedObject) )
+    {
+        emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , value);
+        emit valueChanged(dynamic_cast<dtkAbstractView*>(_attachedObject) , color);
+    }
+
+    emit valueChanged(value);
+}
+
+QIcon medColorListParameter::createIcon(QString colorName)
+{
+    QPixmap iconPixmap(32,32);
+    iconPixmap.fill(QColor(colorName));
+    QIcon itemIcon(iconPixmap);
+    return itemIcon;
+}
+
+
+//------------------ medColorListParameter -------------------------------------
+
+medParameterPool::medParameterPool()
+{
+
+}
+
+medParameterPool::~medParameterPool()
+{
+    _pool.clear();
+}
+
+void medParameterPool::append(medAbstractParameter *parameter)
+{
+    _pool.insert(parameter->name(), parameter);
+    connectParam(parameter);
+}
+
+void medParameterPool::clear()
+{
+    foreach(medAbstractParameter* param, _pool)
+        disconnectParam(param);
+
+    _pool.clear();
+}
+
+void medParameterPool::connectParam(medAbstractParameter *parameter)
+{
+    if(medActionParameter *param = dynamic_cast<medActionParameter*>(parameter))
+        connect(param, SIGNAL(triggered()), this, SLOT(triggerParams()));
+    else if(medBooleanParameter *param = dynamic_cast<medBooleanParameter*>(parameter))
+        connect(param, SIGNAL(valueChanged(bool)), this, SLOT(triggerParams(bool)));
+    else if(medIntParameter *param = dynamic_cast<medIntParameter*>(parameter))
+        connect(param, SIGNAL(valueChanged(int)), this, SLOT(triggerParams(int)));
+    else if(medDoubleParameter *param = dynamic_cast<medDoubleParameter*>(parameter))
+        connect(param, SIGNAL(valueChanged(double)), this, SLOT(triggerParams(double)));
+    else if(medListParameter *param = dynamic_cast<medListParameter*>(parameter))
+        connect(param, SIGNAL(valueChanged(QString)), this, SLOT(triggerParams(QString)));
+    else if(medColorListParameter *param = dynamic_cast<medColorListParameter*>(parameter))
+        connect(param, SIGNAL(valueChanged(QString)), this, SLOT(triggerParams(QString)));
+}
+
+void medParameterPool::disconnectParam(medAbstractParameter *parameter)
+{
+    parameter->disconnect(this);
+}
+
+void medParameterPool::triggerParams()
+{
+    medActionParameter *sender = dynamic_cast<medActionParameter*>(QObject::sender());
+
+    if(!sender)
+        return;
+
+    foreach(medAbstractParameter *param, _pool.values(sender->name()))
+    {
+        medActionParameter *actionParam = dynamic_cast<medActionParameter*>(param);
+        if(actionParam != sender)
+        {
+            disconnectParam(param);
+            actionParam->emitTriggered();
+            connectParam(param);
+        }
+    }
+}
+
+void medParameterPool::triggerParams(bool value)
+{
+    medBooleanParameter *sender = dynamic_cast<medBooleanParameter*>(QObject::sender());
+
+    if(!sender)
+        return;
+
+    foreach(medAbstractParameter *param, _pool.values(sender->name()))
+    {
+        medBooleanParameter *boolParam = dynamic_cast<medBooleanParameter*>(param);
+        if(boolParam != sender)
+            boolParam->setValue(value);
+    }
+}
+
+void medParameterPool::triggerParams(int value)
+{
+    medIntParameter *sender = dynamic_cast<medIntParameter*>(QObject::sender());
+
+    if(!sender)
+        return;
+
+    foreach(medAbstractParameter *param, _pool.values(sender->name()))
+    {
+        medIntParameter *intParam = dynamic_cast<medIntParameter*>(param);
+        if(intParam != sender)
+            intParam->setValue(value);
+    }
+}
+
+void medParameterPool::triggerParams(double value)
+{
+    medDoubleParameter *sender = dynamic_cast<medDoubleParameter*>(QObject::sender());
+
+    if(!sender)
+        return;
+
+    foreach(medAbstractParameter *param, _pool.values(sender->name()))
+    {
+        medDoubleParameter *doubleParam = dynamic_cast<medDoubleParameter*>(param);
+        if(doubleParam != sender)
+            doubleParam->setValue(value);
+    }
+}
+
+void medParameterPool::triggerParams(QString value)
+{
+    medListParameter *sender = dynamic_cast<medListParameter*>(QObject::sender());
+
+    if(!sender)
+        return;
+
+    foreach(medAbstractParameter *param, _pool.values(sender->name()))
+    {
+        medListParameter *listParam = dynamic_cast<medListParameter*>(param);
+        if(listParam != sender)
+            listParam->setDefaultValue(value);
+    }
 }
--- src/medGui/medParameter.h
+++ src/medGui/medParameter.h
@@ -6,41 +6,116 @@
 #include <dtkCore/dtkAbstractObject.h>
 #include <dtkCore/dtkAbstractProcess.h>
 #include <dtkCore/dtkAbstractData.h>
+#include <dtkCore/dtkAbstractView.h>
 
+class medParameterPool;
 
 class medAbstractParameter : public QObject
 {
     Q_OBJECT
 
 public:
-    medAbstractParameter(QString name, dtkAbstractObject* attachedData = 0) : _name(name), _widget(0), _attachedData(attachedData)
-    {
-        _widget = new QWidget;
-        _layout = new QHBoxLayout(_widget);
-        _label = new QLabel(name);
-        _layout->addWidget(_label);
+    medAbstractParameter(QString name, dtkAbstractObject* attachedObject = 0)
+        : _name(name), _widget(0), _attachedObject(attachedObject){}
 
-        _layout->setContentsMargins(1,1,1,1);
-    }
     virtual ~medAbstractParameter() {delete _widget;}
 
     QString name() const {return _name;}
 
     QWidget* getWidget() {if (!_widget) setupWidget(); return _widget;}
 
-    // pour les medParamPools ? ptet pas utile
-    //virtual void sync(medAbstractParam * p) = 0;
+    bool match(medAbstractParameter const *other)
+    {
+        return this->name()==other->name();
+    }
+
+    void hideLabel(){ _label->hide() ;}
 
 protected:
     virtual void setupWidget() = 0;
+
+    void initializeWidget()
+    {
+        _widget = new QWidget;
+        _layout = new QHBoxLayout(_widget);
+        _label = new QLabel(_name);
+        _layout->addWidget(_label);
+
+        _layout->setContentsMargins(1,1,1,1);
+
+        connect(_widget, SIGNAL(destroyed()), this, SLOT(deleteWidget()));
+    }
 
     QString       _name;
     QWidget *     _widget;
     QLabel *      _label;
     QHBoxLayout * _layout;
-    dtkAbstractObject *_attachedData;
+    dtkAbstractObject *_attachedObject;
+
+private slots:
+    void deleteWidget(){_widget=0;}
 
 };
+
+
+class medActionParameter : public medAbstractParameter
+{
+    Q_OBJECT
+
+public:
+    medActionParameter(QString name, dtkAbstractObject* attachedObject = 0);
+
+    void setIcon(QIcon icon);
+    void setText(QString text);
+    void setCheckable(bool value);
+
+public slots:
+    void emitTriggered(bool value = true);
+
+signals:
+    void triggered();
+    void triggered(dtkAbstractData * attachedData);
+    void triggered(dtkAbstractProcess * attachedProcess);
+    void triggered(dtkAbstractView * attachedProcess);
+
+    void triggered(bool);
+    void triggered(dtkAbstractData * attachedData, bool);
+    void triggered(dtkAbstractProcess * attachedProcess, bool);
+    void triggered(dtkAbstractView * attachedProcess, bool);
+
+protected:
+    void setupWidget();
+
+
+private:
+    QPushButton * _button;
+
+};
+
+
+class medActionParameterGroup : QObject
+{
+    Q_OBJECT
+
+public:
+    medActionParameterGroup(QWidget *parent){ _buttonGroup = new QButtonGroup(parent);}
+
+    void addActionParameter(medActionParameter *action)
+    {
+        const QObjectList children = action->getWidget()->children();
+        foreach(QObject *child, children)
+        {
+            QPushButton *button = dynamic_cast<QPushButton *>(child);
+            if( button )
+                _buttonGroup->addButton(button);
+        }
+    }
+    void setExclusive(bool exclusive){_buttonGroup->setExclusive(exclusive);}
+
+private:
+    QButtonGroup *_buttonGroup;
+};
+
 
 
 class medBooleanParameter : public medAbstractParameter
@@ -48,7 +123,7 @@ class medBooleanParameter : public medAbstractParameter
     Q_OBJECT
 
 public:
-    medBooleanParameter(QString name, dtkAbstractObject* attachedData = 0);
+    medBooleanParameter(QString name, dtkAbstractObject* attachedObject = 0);
 
     void setValue(bool value);
     bool value();
@@ -57,6 +132,7 @@ signals:
     void valueChanged(bool value);
     void valueChanged(dtkAbstractData * attachedData, bool value);
     void valueChanged(dtkAbstractProcess * attachedProcess, bool value);
+    void valueChanged(dtkAbstractView * attachedView, bool value);
 
 protected:
     void setupWidget();
@@ -74,7 +150,7 @@ class medDoubleParameter: public medAbstractParameter
     Q_OBJECT
 
 public:
-    medDoubleParameter(QString name, dtkAbstractObject* attachedData = 0);
+    medDoubleParameter(QString name, dtkAbstractObject* attachedObject = 0);
 
     void setValue(double value);
     double value();
@@ -85,6 +161,7 @@ signals:
     void valueChanged(double value);
     void valueChanged(dtkAbstractData * attachedData, double value);
     void valueChanged(dtkAbstractProcess * attachedProcess, double value);
+    void valueChanged(dtkAbstractView * attachedView, double value);
 
 protected:
     void setupWidget();
@@ -103,7 +180,7 @@ class medIntParameter : public medAbstractParameter
     Q_OBJECT
 
 public:
-    medIntParameter(QString name, dtkAbstractObject* attachedData = 0);
+    medIntParameter(QString name, dtkAbstractObject* attachedObject = 0);
 
     void setValue(int value);
     int value();
@@ -114,6 +191,7 @@ signals:
     void valueChanged(int value);
     void valueChanged(dtkAbstractData * attachedData, int value);
     void valueChanged(dtkAbstractProcess * attachedProcess, int value);
+    void valueChanged(dtkAbstractView * attachedView, int value);
 
 protected:
     void setupWidget();
@@ -131,7 +209,7 @@ class medListParameter : public medAbstractParameter
     Q_OBJECT
 
 public:
-    medListParameter(QString name, dtkAbstractObject* attachedData = 0);
+    medListParameter(QString name, dtkAbstractObject* attachedObject = 0);
 
     void setValues(QStringList values);
     void setDefaultValue(QString value);
@@ -141,15 +219,88 @@ signals:
     void valueChanged(QString value);
     void valueChanged(dtkAbstractData * attachedData, QString value);
     void valueChanged(dtkAbstractProcess * attachedProcess, QString value);
+    void valueChanged(dtkAbstractView * attachedView, QString value);
+    void valueChanged(int value);
+    void valueChanged(dtkAbstractData * attachedData, int value);
+    void valueChanged(dtkAbstractProcess * attachedProcess, int value);
+    void valueChanged(dtkAbstractView * attachedView, int value);
 
 protected:
     void setupWidget();
 
 private slots:
     void emitValueChanged(QString);
+    void emitValueChanged(int value);
 
 private:
     QComboBox *_comboBox;
+};
+
+
+class medColorListParameter : public medAbstractParameter
+{
+    Q_OBJECT
+
+public:
+    medColorListParameter(QString name, dtkAbstractObject* attachedObject = 0);
+
+    void setValues(QList<QColor> values);
+    void setValues(QStringList values);
+
+    void setDefaultValue(QColor value);
+    void setDefaultValue(QString value);
+
+    QString value();
+
+signals:
+    void valueChanged(QColor value);
+    void valueChanged(dtkAbstractData * attachedData, QColor value);
+    void valueChanged(dtkAbstractProcess * attachedProcess, QColor value);
+    void valueChanged(dtkAbstractView * attachedView, QColor value);
+
+    void valueChanged(QString value);
+    void valueChanged(dtkAbstractData * attachedData, QString value);
+    void valueChanged(dtkAbstractProcess * attachedProcess, QString value);
+    void valueChanged(dtkAbstractView * attachedView, QString value);
+
+protected:
+    void setupWidget();
+    QIcon createIcon(QString colorName);
+
+private slots:
+    void emitValueChanged(QString);
+
+private:
+    QComboBox *_comboBox;
+};
+
+
+class medParameterPool : public QObject
+{
+    Q_OBJECT
+
+public:
+    medParameterPool();
+    virtual ~medParameterPool();
+
+    void append(medAbstractParameter *parameter);
+    void clear();
+    int count(){return _pool.count();}
+
+private slots:
+    void triggerParams();
+    void triggerParams(bool);
+    void triggerParams(int);
+    void triggerParams(double);
+    void triggerParams(QString);
+
+private:
+    void connectParam(medAbstractParameter *parameter);
+    void disconnectParam(medAbstractParameter *parameter);
+
+private:
+    QMultiMap<QString, medAbstractParameter*> _pool;
+
 };
 
 #endif // MEDPARAMETERFACTORY_H
