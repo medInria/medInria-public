e4a68fe307e3802846f6746553221d97cdf67aa9
Olivier Commowick <olivier.commowick@inria.fr>
2013-11-20 16:06:26 +0100

    Add itkThumbnailGenerator, works for 3D and 4D scalar images

---
+++ src-plugins/itkDataImage/itkThumbnailGenerator.cpp
@@ -0,0 +1,100 @@
+#include <itkThumbnailGenerator.h>
+
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkUnsignedCharArray.h>
+#include <QVTKWidget.h>
+
+itkThumbnailGenerator::itkThumbnailGenerator(medAbstractData *data, unsigned int dimension)
+{
+    m_XYDim = 128;
+    m_Thumbnail = QImage(m_XYDim,m_XYDim,QImage::Format_RGB32);
+    m_Thumbnail.fill(0);
+    m_InputData = 0;
+    
+    if (data->identifier().contains("itkDataImage"))
+        m_InputData = data;
+    
+    m_ImageDimension = dimension;
+    
+    this->moveToThread(QApplication::instance()->thread());
+}
+
+QImage &itkThumbnailGenerator::thumbnail()
+{
+    if (QThread::currentThread() != QApplication::instance()->thread())
+        QMetaObject::invokeMethod(this, "generateThumbnail", Qt::BlockingQueuedConnection);
+    else
+        generateThumbnail();
+
+    return m_Thumbnail;
+}
+
+void itkThumbnailGenerator::generateThumbnail()
+{
+    if (!m_InputData)
+        return;
+
+    vtkSmartPointer <vtkRenderer> renderer2d = vtkRenderer::New();
+    
+    vtkSmartPointer <vtkImageView2D> view2d = vtkImageView2D::New();
+    view2d->SetRenderer (renderer2d);
+    view2d->SetBackground ( 0.0, 0.0, 0.0 );
+    view2d->CursorFollowMouseOff();
+    view2d->ShowImageAxisOff();
+    view2d->ShowScalarBarOff();
+    view2d->ShowAnnotationsOff();
+    view2d->ShowRulerWidgetOff();
+    
+    vtkSmartPointer <vtkRenderWindow> renWin = vtkRenderWindow::New();
+    renWin->SetOffScreenRendering(1);
+    view2d->SetRenderWindow (renWin);
+    
+    QVTKWidget *vtkWidget = new QVTKWidget;
+    vtkWidget->resize(m_XYDim,m_XYDim);
+    vtkWidget->SetRenderWindow(renWin);
+    
+    if (m_ImageDimension == 3)
+    {
+        typedef itk::ImageBase<3> ImageType;
+        
+        ImageType::Pointer imgPtr = dynamic_cast <ImageType *> ((itk::Object *)m_InputData->data());
+        
+        this->SetViewInput<double> (imgPtr,view2d);
+        this->SetViewInput<float> (imgPtr,view2d);
+        this->SetViewInput<int> (imgPtr,view2d);
+        this->SetViewInput<unsigned int> (imgPtr,view2d);
+        this->SetViewInput<short> (imgPtr,view2d);
+        this->SetViewInput<unsigned short> (imgPtr,view2d);
+        this->SetViewInput<long> (imgPtr,view2d);
+        this->SetViewInput<unsigned long> (imgPtr,view2d);
+        this->SetViewInput<char> (imgPtr,view2d);
+        this->SetViewInput<unsigned char> (imgPtr,view2d);
+    }
+    else
+    {
+        typedef itk::ImageBase<4> ImageType;
+        
+        ImageType::Pointer imgPtr = dynamic_cast <ImageType *> ((itk::Object *)m_InputData->data());
+        
+        this->SetViewInput4<double> (imgPtr,view2d);
+        this->SetViewInput4<float> (imgPtr,view2d);
+        this->SetViewInput4<int> (imgPtr,view2d);
+        this->SetViewInput4<unsigned int> (imgPtr,view2d);
+        this->SetViewInput4<short> (imgPtr,view2d);
+        this->SetViewInput4<unsigned short> (imgPtr,view2d);
+        this->SetViewInput4<long> (imgPtr,view2d);
+        this->SetViewInput4<unsigned long> (imgPtr,view2d);
+        this->SetViewInput4<char> (imgPtr,view2d);
+        this->SetViewInput4<unsigned char> (imgPtr,view2d);
+    }
+    
+    view2d->Reset();
+    view2d->Render();
+    
+    vtkSmartPointer <vtkUnsignedCharArray> pixels = vtkUnsignedCharArray::New();
+    pixels->SetArray(m_Thumbnail.bits(), m_XYDim*m_XYDim*4, 1);
+    renWin->GetRGBACharPixelData(0, 0, m_XYDim-1, m_XYDim-1, 1, pixels);
+    
+    m_Thumbnail = m_Thumbnail.mirrored(false,true);
+}
