cfcf95aff11ab36a9c7fa0f4b41e67dbf4664705
Florian Vichot <florian.vichot@inria.fr>
2014-04-09 11:09:41 +0200

    Fix reorder warning in medVtkViewBackend

--- src/medVtkInria/vtkImageView/medVtkViewBackend.cpp
+++ src/medVtkInria/vtkImageView/medVtkViewBackend.cpp
@@ -3,6 +3,6 @@
 
 medVtkViewBackend::medVtkViewBackend(vtkImageView2D *view2D_,vtkImageView3D *view3D_,
                                      vtkRenderer *renderer2D_, vtkRenderer *renderer3D_, vtkRenderWindow *renWin_)
-    : view2D(view2D_),view3D(view3D_),renWin(renWin_), renderer2D(renderer2D_), renderer3D(renderer3D_)
+    : view2D(view2D_),view3D(view3D_),renderer2D(renderer2D_),renderer3D(renderer3D_),renWin(renWin_)
 {
 }
