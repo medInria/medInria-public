a68bd510b6cd11f398b6c244f0026f8530b76bbf
Guillaume Pasquier <guillaume.pasquier@inria.fr>
2014-10-22 11:56:48 +0200

    moving itkProcessDiffeomorphicDeamons to new interface

--- src-plugins/itkProcessRegistrationDiffeomorphicDemons/itkProcessRegistrationDiffeomorphicDemons.h
+++ src-plugins/itkProcessRegistrationDiffeomorphicDemons/itkProcessRegistrationDiffeomorphicDemons.h
@@ -30,115 +30,25 @@ class ITKPROCESSREGISTRATIONDIFFEOMORPHICDEMONSPLUGIN_EXPORT itkProcessRegistrat
     Q_OBJECT
 
 public:
-    /**
-     * @brief Constructor.
-     *
-     * @param void
-     */
-    itkProcessRegistrationDiffeomorphicDemons();
 
-    /**
-     * @brief
-     *
-     * @param void
-    */
+    itkProcessRegistrationDiffeomorphicDemons();
     virtual ~itkProcessRegistrationDiffeomorphicDemons();
 
-    /**
-     * @brief Description of the plugin.
-     *
-     * @param void
-     * @return QString
-     */
     virtual QString description() const;
-
-    /**
-     * @brief Identifier of the plugin.
-     *
-     * @param void
-     * @return QString
-     */
     virtual QString identifier() const;
 
-    /**
-     * @brief tries to register the process with the factory.
-     *
-     * @param void
-     * @return bool true if it succeeded, false otherwise.
-    */
     static bool registered();
 
 public:
-    /**
-     * @brief Runs the process.
-     *
-     * @param ImageType the fixed image image type.
-     * @return int successful or not.
-    */
     virtual int update(ImageType);
 
-    /**
-     * @brief
-     *
-     * @see rpiDiffeomorphicDemons
-     *
-     * @param iterations
-    */
     void setNumberOfIterations(std::vector <unsigned int> iterations);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param updateRule
-    */
-    void setUpdateRule( unsigned char updateRule);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param gradientType
-    */
-    void setGradientType (unsigned char gradientType);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param maximumUpdateStepLength
-    */
-    void setMaximumUpdateLength (float maximumUpdateStepLength);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param updateFieldStandardDeviation
-    */
-    void setUpdateFieldStandardDeviation(float updateFieldStandardDeviation);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param displacementFieldStandardDeviation
-    */
-    void setDisplacementFieldStandardDeviation(float displacementFieldStandardDeviation);
-
-    /**
-     * @brief
-     * @see rpiDiffeomorphicDemons
-     * @param useHistogramMatching
-    */
-    void setUseHistogramMatching(bool useHistogramMatching);
 
     virtual itk::Transform<double,3,3>::Pointer getTransform();
     virtual QString getTitleAndParameters();
 
+
 protected :
-    /**
-     * @brief Writes the transformation, in this case the displacement field,
-     * in a file.
-     *
-     * @param file The path to the file is assumed to be existing. However the file may not exist beforehand.
-     * @return bool successful or not.
-    */
     virtual bool writeTransform(const QString& file);
 
 private:
@@ -146,12 +56,7 @@ private:
     friend class itkProcessRegistrationDiffeomorphicDemonsPrivate;
 };
 
-/**
- * @brief Function to instantiate the process from the factory.
- * @see registered()
- *
- * @param void
-*/
+
 dtkAbstractProcess *createitkProcessRegistrationDiffeomorphicDemons();
 
 
