b567bfc0cf139ec3c12bfa20f9ca4033c3034a79
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-12-04 12:05:28 +0100

    Remove redundant code.

--- app/medInria/medMainWindow.cpp
+++ app/medInria/medMainWindow.cpp
@@ -425,14 +425,6 @@ void medMainWindow::setFullScreen (const bool full)
         this->showNormal();
 }
 
-void medMainWindow::toggleFullScreen()
-{
-    if ( !this->isFullScreen())
-        this->showFullScreen();
-    else
-        this->showNormal();
-}
-
 void medMainWindow::saveScene() {
 }
 
