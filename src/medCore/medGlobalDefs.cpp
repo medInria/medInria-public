5f7db4e043c81fe527ff2ef9e367809e3765b4d7
Florian Vichot <florian.vichot@inria.fr>
2014-12-11 11:55:35 +0100

    Simplify implementation of smartBaseName

--- src/medCore/medGlobalDefs.cpp
+++ src/medCore/medGlobalDefs.cpp
@@ -1,19 +1,21 @@
 #include <medGlobalDefs.h>
 
-#include <QRegExp>
+#include <QFileInfo>
 
 namespace med {
 
 QString smartBaseName(const QString & fileName) {
     // remove extension after the first dot, or if it ends in a compression extension,
     // the one before. So "test.nii.gz" -> "test" and "test.mha" -> "test"
-
-    QRegExp r1("^(.+)(?:\\.[^.]+(?:\\.(?:gz|Z|bz2)))$", Qt::CaseInsensitive, QRegExp::RegExp2);
-    QRegExp r2("^(.+)(?:\\.[^.]+)$", Qt::CaseInsensitive, QRegExp::RegExp2);
-    r1.setMinimal(true);
-    r2.setMinimal(true);
-
-    return r1.exactMatch(fileName) ? r1.cap(1) : (r2.exactMatch(fileName) ? r2.cap(1) : fileName);
+    QFileInfo info(fileName);
+    if( info.suffix().toLower() == "gz"
+     || info.suffix().toLower() == "z"
+     || info.suffix().toLower() == "bz2")
+    {
+        QFileInfo infoWithoutCompression(info.completeBaseName());
+        return infoWithoutCompression.completeBaseName();
+    }
+    else return info.completeBaseName();
 }
 
 }
