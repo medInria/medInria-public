63b29c91ea0e378500868d08aff0ff460ee42216
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2015-07-16 20:02:56 +0200

    Activate medCoreLegacy and add new medCore which currently only forwards to medCoreLegacy (failed to introduce namespaces yet!)

---
+++ src/medCore/gui/tests/medViewContainersTest.h
@@ -0,0 +1,12 @@
+//  medInria
+//
+//  Copyright (c) INRIA 2013 - 2015. All rights reserved.
+//  See LICENSE.txt for details.
+// 
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.
+
+#pragma once
+
+#include <medCoreLegacy/./gui/tests/medViewContainersTest.h>
