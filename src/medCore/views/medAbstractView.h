9f2923dd7bfeb21965068b7b3265601d716dfa8c
Loic Cadour <loic.cadour@inria.fr>
2015-07-24 17:33:21 +0200

    extraNavigators back to protected

--- src/medCore/views/medAbstractView.h
+++ src/medCore/views/medAbstractView.h
@@ -61,7 +61,6 @@ public:
     virtual QWidget* toolBarWidget();
 
     virtual QList<medAbstractNavigator*> navigators();
-    virtual QList<medAbstractNavigator*> extraNavigators();
     virtual QList<medAbstractInteractor*> interactors();
 
     medDoubleParameter* zoomParameter();
@@ -97,6 +96,7 @@ protected:
     virtual medAbstractViewInteractor* primaryInteractor();
     virtual QList<medAbstractInteractor*> extraInteractors();
     virtual medAbstractViewNavigator* primaryNavigator();
+    virtual QList<medAbstractNavigator*> extraNavigators();
 
     virtual bool initialiseInteractors(medAbstractData* data);
     virtual bool initialiseNavigators();
