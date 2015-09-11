161dde1389d6d553d509487a67c15d484c73f44b
René-paul Debroize <rene-paul.debroize@irisa.fr>
2015-09-11 16:46:05 +0200

    Add medItkOpeningImageProcess.

---
+++ src-plugins/process/morphomath_operation/medItkOpeningImageProcess/medItkOpeningImageProcessPlugin.h
@@ -0,0 +1,31 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+#pragma once
+
+#include <medAbstractOpeningImageProcess.h>
+
+class MEDINRIA_EXPORT medItkOpeningImageProcessPlugin : public medAbstractOpeningImageProcessPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(medAbstractOpeningImageProcessPlugin)
+    Q_PLUGIN_METADATA(IID "fr.inria.medItkOpeningImageProcessPlugin" FILE "medItkOpeningImageProcessPlugin.json")
+
+public:
+     medItkOpeningImageProcessPlugin(void) {}
+    virtual ~medItkOpeningImageProcessPlugin(void) {}
+
+public:
+    void initialize(void);
+    void uninitialize(void);
+};
