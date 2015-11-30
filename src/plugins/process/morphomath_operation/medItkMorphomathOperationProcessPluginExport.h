8e6ea9786f8d588179d7a4d18735f700e090578d
rdebroiz <rene-paul.debroize@inria.fr>
2015-11-30 15:00:44 +0100

    add missing export files.

---
+++ src/plugins/process/morphomath_operation/medItkMorphomathOperationProcessPluginExport.h
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+#pragma once
+
+#include <QtGlobal>
+
+#if defined(medItkMorphomathOperationProcessPlugin_EXPORTS)
+#  define MEDITKMORPHOMATHOPERATIONPROCESSPLUGIN_EXPORT Q_DECL_EXPORT
+#else
+#  define MEDITKMORPHOMATHOPERATIONPROCESSPLUGIN_EXPORT Q_DECL_IMPORT
+#endif
+
+
+
+
