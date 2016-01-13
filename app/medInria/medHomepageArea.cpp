2a0ccde7dac56761873fad41e2216cee7fe43aae
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2016-01-13 14:43:43 +0100

    [expiryDate] minus '()'

--- app/medInria/medHomepageArea.cpp
+++ app/medInria/medHomepageArea.cpp
@@ -137,9 +137,9 @@ medHomepageArea::medHomepageArea ( QWidget * parent ) : QWidget ( parent ), d ( 
                                           "<br/><br/>"
                                           "<b>MUSIC</b> is proprietary software, copyright (c) 2014-2015, IHU Liryc, Université de Bordeaux and Inria."
                                           "<br/><br/>"
-                                          " <font color = 'red'><b>MUSIC (expires on ")
+                                          " <font color = 'red'><b>MUSIC expires on ")
                         + expiryDate.toString("d MMMM yyyy")
-                        + ")</b></font>");
+                        + ".</b></font>");
 
     textEdit->setReadOnly ( true );
     textEdit->setFocusPolicy ( Qt::NoFocus );
