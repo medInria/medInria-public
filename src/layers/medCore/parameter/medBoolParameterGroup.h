5e3f5db9f407eb1ce7c6872cb1c9d6bf6eae792a
Olivier Commowick <olivier.commowick@inria.fr>
2016-03-22 17:18:59 +0100

    Add a new class for bool group logic

---
+++ src/layers/medCore/parameter/medBoolParameterGroup.h
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+#pragma once
+
+#include <QObject>
+#include <medCoreExport.h>
+
+class medBoolParameter;
+class medBoolParameterGroupPrivate;
+
+class MEDCORE_EXPORT medBoolParameterGroup : public QObject
+{
+    Q_OBJECT
+
+public:
+    medBoolParameterGroup(QObject *parent = NULL);
+    virtual ~medBoolParameterGroup();
+
+    void addBoolParameter(medBoolParameter *param);
+    QList <medBoolParameter *> &parameters();
+
+public slots:
+    void changeActiveBoolParameter(bool value);
+
+private:
+    const QScopedPointer<medBoolParameterGroupPrivate> d;
+};
