55bbe5d9daa73823589264df0511c0da7d733504
Olivier Commowick <olivier.commowick@inria.fr>
2016-04-07 17:59:48 +0200

    Correct patch application

--- superbuild/configuration_steps/EP_GeneratePatchCommand.cmake
+++ superbuild/configuration_steps/EP_GeneratePatchCommand.cmake
@@ -3,20 +3,21 @@
 ## #############################################################################
 
 function(ep_GeneratePatchCommand ep OutVar)
+    find_program(GIT_BIN NAMES git)
     foreach (patch ${ARGN})
-        execute_process(COMMAND "git apply --ignore-whitespace  --check ${CMAKE_SOURCE_DIR}/patches/${patch}"
+        execute_process(COMMAND ${GIT_BIN} apply --ignore-whitespace  --check ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch}
                         WORKING_DIRECTORY ${EP_PREFIX_thirdparts}/src/${ep}
-                        RESULT_VARIABLE   PATCH_OK
+                        RESULT_VARIABLE PATCH_OK
                         OUTPUT_QUIET
                         ERROR_QUIET)
         if (PATCH_OK EQUAL 0 OR NOT EXISTS ${EP_PREFIX_thirdparts}/src/${ep})
-            set(PATCHES_TO_APPLY ${PATCHES_TO_APPLY} ${CMAKE_CURRENT_SOURCE_DIR}/patches/${patch})
+            set(PATCHES_TO_APPLY ${PATCHES_TO_APPLY} ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch})
         endif()
     endforeach()
 
     set(PATCH_COMMAND)
     if (NOT "${PATCHES_TO_APPLY}" STREQUAL "")
-        set(PATCH_COMMAND "git apply --ignore-whitespace ${PATCHES_TO_APPLY}")
+        set(PATCH_COMMAND ${GIT_BIN} apply --ignore-whitespace ${PATCHES_TO_APPLY})
     endif()
 
     set(${OutVar} ${PATCH_COMMAND} PARENT_SCOPE)
