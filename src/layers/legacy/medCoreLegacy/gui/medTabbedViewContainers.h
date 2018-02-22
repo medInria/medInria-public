3de14ad57eb134125a33737c37141f1863fdcfeb
Florent2305 <florent.leray@hotmail.fr>
2018-02-22 15:54:40 +0100

    update copyrights

--- src/layers/legacy/medCoreLegacy/gui/medTabbedViewContainers.h
+++ src/layers/legacy/medCoreLegacy/gui/medTabbedViewContainers.h
@@ -3,7 +3,7 @@
 
  medInria
 
- Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ Copyright (c) INRIA 2013 - 2018. All rights reserved.
  See LICENSE.txt for details.
 
   This software is distributed WITHOUT ANY WARRANTY; without even
