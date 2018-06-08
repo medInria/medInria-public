201cbf42d98f83b7a88e260739feffa8445d0a1c
Olivier Commowick <olivier.commowick@inria.fr>
2018-06-08 15:39:49 +0200

    Correct ssh key gitlab

--- superbuild/environment/GitlabInriaSshTest.cmake
+++ superbuild/environment/GitlabInriaSshTest.cmake
@@ -15,7 +15,7 @@
 ## Add gitlab.inria.fr's SSH signature to the .ssh/known_hosts file
 ## #############################################################################
 
-set(GITLAB_INRIA_SIGN "gitlab.inria.fr ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+set(GITLAB_INRIA_SIGN "gitlab.inria.fr ssh-rsa AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAFldY2ft1ReZjFYPpe/wa5Vhl4YJfYz6IJOZaZxL924R44jrGWiR2/Misrug75NBsQB+UWs1iIyWZPk0AN45Sk="
   )
 
 file(TO_CMAKE_PATH ${HOME_PATH} HOME_PATH)
