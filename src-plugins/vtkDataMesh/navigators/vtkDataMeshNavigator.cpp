177a899f38d634068887162a469dbeb361d2140f
Mehdi Juhoor <mehdi.juhoor@inria.fr>
2018-12-12 15:02:44 +0100

    [Depth peeling] Reduce number of maximum peels (#495)
    * Reduce number of maximum peels.
    
    * Increase maximum number of peels. Increase occlusion ratio.

--- src-plugins/vtkDataMesh/navigators/vtkDataMeshNavigator.cpp
+++ src-plugins/vtkDataMesh/navigators/vtkDataMeshNavigator.cpp
@@ -151,8 +151,8 @@ void vtkDataMeshNavigator::enableDepthPeeling(bool enabled)
     {
         // Activate depth-peeling to have a proper opacity rendering
         d->view3d->GetRenderer()->SetUseDepthPeeling(1);
-        d->view3d->GetRenderer()->SetMaximumNumberOfPeels(100);
-        d->view3d->GetRenderer()->SetOcclusionRatio(0.01);
+        d->view3d->GetRenderer()->SetMaximumNumberOfPeels(10);
+        d->view3d->GetRenderer()->SetOcclusionRatio(0.03);
 
     }
     else d->view3d->GetRenderer()->SetUseDepthPeeling(0);
