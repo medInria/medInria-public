a13ba46af3063a03bb7e6e1e2c3f7c1e73be72eb
Olivier Commowick <olivier.commowick@inria.fr>
2019-04-01 11:30:16 +0200

    No OSX rpath for VTK

--- superbuild/projects_modules/VTK.cmake
+++ superbuild/projects_modules/VTK.cmake
@@ -57,6 +57,7 @@ set(cmake_args
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE_thirdparts}
   -DCMAKE_C_FLAGS=${${ep}_c_flags}
   -DCMAKE_CXX_FLAGS=${${ep}_cxx_flags}
+  -DCMAKE_MACOSX_RPATH:BOOL=OFF
   -DCMAKE_SHARED_LINKER_FLAGS=${${ep}_shared_linker_flags}  
   -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>  
   -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS_${ep}}
