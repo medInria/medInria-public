f0f889e16f3e95ba4f40bdcc43ee3be034d713fd
Florent LERAY <florent.leray@inria.fr>
2019-06-03 11:21:19 +0200

    introduce medFilteringWorkspaceL

---
+++ src/app/medInria/medFilteringWorkspaceL.h
@@ -0,0 +1,62 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ See LICENSE.txt for details.
+ 
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+#pragma once
+
+#include <QtCore>
+#include <medAbstractWorkspaceLegacy.h>
+
+class medFilteringWorkspaceLPrivate;
+class medViewContainerStack;
+class medAbstractData;
+class dtkAbstractView;
+
+/**
+ * @brief Workspace providing a comparative display of the input and output of image-to-image filtering process plugins
+ */
+class medFilteringWorkspaceL : public medAbstractWorkspaceLegacy
+{
+    Q_OBJECT
+    MED_WORKSPACE_INTERFACE("FilteringLegacy",
+                            "Workspace to apply filters to images.",
+                            "Methodology")
+public:
+    medFilteringWorkspaceL(QWidget *parent = 0);
+    ~medFilteringWorkspaceL();
+
+    static bool isUsable();
+
+    void setupViewContainerStack ();
+
+    virtual void open(const medDataIndex &index);
+
+signals:
+
+    /**
+     * @brief signal emitted to refresh the output view with the data resulting from a successful filtering process
+     *
+     * This is a connection between the medFilteringSelectorToolBox and the medFilteringViewContainer which displays input/output images
+     *
+     */
+    void outputDataChanged ( medAbstractData * );
+
+protected slots:
+
+    void changeToolBoxInput();
+    void onProcessSuccess();
+
+private:
+    medFilteringWorkspaceLPrivate *d;
+};
+
+
