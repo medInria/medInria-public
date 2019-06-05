1af596f8ab08413b50cdc50a4f736d5972b4d290
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2019-06-05 17:12:51 +0200

    [music3] links plugins non-legacy in Linux

--- src/app/medInria/medApplication.cpp
+++ src/app/medInria/medApplication.cpp
@@ -198,8 +198,10 @@ void medApplication::initialize()
     QDir plugins_dir;
 #ifdef Q_OS_MAC
     plugins_dir = qApp->applicationDirPath() + "/../PlugIns";
-#else
+#elif Q_OS_WIN
     plugins_dir = qApp->applicationDirPath() + "/../plugins";
+#else
+    plugins_dir = qApp->applicationDirPath() + "/plugins";
 #endif
     defaultPath = plugins_dir.absolutePath();
 
