c4026d9d0fba3708d397b0086da72cff8f31fb99
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2019-07-18 15:45:25 +0200

    [music3] copyrights 2019

--- src/plugins/legacy/medBinaryOperation/medBinaryOperatorBase.cpp
+++ src/plugins/legacy/medBinaryOperation/medBinaryOperatorBase.cpp
@@ -1,3 +1,15 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2019. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
 
 #include "medBinaryOperatorBase.h"
 
