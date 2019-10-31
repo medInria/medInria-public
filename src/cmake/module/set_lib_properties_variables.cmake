b109bb74e51a74446c3d02c63bda3ff4cfed89d5
Olivier Commowick <olivier.commowick@inria.fr>
2019-10-31 10:47:20 +0100

    Correct variables export/import for windows

--- src/cmake/module/set_lib_properties_variables.cmake
+++ src/cmake/module/set_lib_properties_variables.cmake
@@ -26,9 +26,19 @@ macro(set_lib_properties_variables
 ## Get include directories properties
 ## #############################################################################
 
+
+get_property(GENERATOR_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 foreach(target_name ${ARGV})
     get_property(${target_name}_INCLUDE_DIRS TARGET ${target_name} PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
-    get_property(${target_name}_LIBRARY_DIR TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY)
+	
+	if(${GENERATOR_MULTI_CONFIG})	  
+	  get_property(${target_name}_LIBRARY_DIR_DEBUG          TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY_DEBUG         )
+	  get_property(${target_name}_LIBRARY_DIR_RELEASE        TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY_RELEASE       )
+	  get_property(${target_name}_LIBRARY_DIR_MINSIZEREL     TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY_MINSIZEREL    )
+	  get_property(${target_name}_LIBRARY_DIR_RELWITHDEBINFO TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO)
+	else()
+		get_property(${target_name}_LIBRARY_DIR TARGET ${target_name} PROPERTY LIBRARY_OUTPUT_DIRECTORY)
+	endif()
 endforeach()
 
 endmacro()
