25df29fd53784ec3106f86fd4573c08ab2d5afe3
nebatmusic <nordine.el-baraka@ihu-liryc.fr>
2019-11-18 13:13:01 +0100

    use gvirtualXray 1.1, uncomment openssl if you need it in your plugin but since supports only make -j1 : compile before without with make -jn and then make -j1

--- superbuild/projects_modules/gvirtualXRay.cmake
+++ superbuild/projects_modules/gvirtualXRay.cmake
@@ -39,27 +39,24 @@ if (UNIX OR APPLE)
   set(${ep}_cxx_flags "${${ep}_cxx_flags}")
 endif()
 
-
-message(${CMAKE_CURRENT_SOURCE_DIR})
-
 set(cmake_args
-        ${ep_common_cache_args}
-        -DCMAKE_C_FLAGS:STRING=${${ep}_c_flags}
-        -DCMAKE_CXX_FLAGS:STRING=${${ep}_cxx_flags}
-    -DCMAKE_SHARED_LINKER_FLAGS:STRING=${${ep}_shared_linker_flags}
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DBUILD_TUTORIALS:bool=OFF
-    -DBUILD_EXAMPLES:bool=OFF 
-    #-DNDEBUG
--DGLEW_INCLUDE_DIR=${EP_PATH_SOURCE}/glew/include/
--DZLIB_INCLUDE_DIR=/usr/include/
--U HAS_ITK
--DITK_DIR=/  
-#-DGLEW_LIBRARY="${CMAKE_CURRENT_SOURCE_DIR}/build/glew/build/lib/libGLEW.${LIB_EXT}"
--DXCOM_PATH=${EP_PATH_SOURCE}/../build/XCOM
--DCMAKE_INSTALL_PREFIX:PATH=${EP_PATH_SOURCE}/../build/gvirtualXRay
+${ep_common_cache_args}
+-DCMAKE_C_FLAGS:STRING=${${ep}_c_flags}
+-DCMAKE_CXX_FLAGS:STRING=${${ep}_cxx_flags}
+-DCMAKE_SHARED_LINKER_FLAGS:STRING=${${ep}_shared_linker_flags}
+-DBUILD_TUTORIALS:bool=OFF
+-DBUILD_EXAMPLES:bool=OFF
+-DUSE_ITK:bool=OFF
+-DCMAKE_INSTALL_PREFIX:PATH=${EP_PATH_SOURCE}/../build/gvxr/
+-DUSE_SYSTEM_ZLIB:bool=ON
+-DUSE_SYSTEM_LIBTIFF:bool=OFF
+-DUSE_LIBTIFF:bool=OFF
+-DUSE_FFTW3:bool=OFF
+-DUSE_FREETYPE:bool=OFF
+-DUSE_GDCM:bool=OFF
+-DUSE_ITK:bool=OFF
+-DUSE_assimp=OFF
 )
-
 
 if(UNIX) 
 	set(cmake_args ${cmake_args} 
@@ -67,7 +64,6 @@ if(UNIX)
             -DGLEW_LIBRARY_DIR="${EP_PATH_SOURCE}/../build/glew/build/lib/"
 	    -DCMAKE_BUILD_TYPE=RELEASE
             -DZLIB_LIBRARY_RELEASE="/lib/x86_64-linux-gnu/libz.so.1"
-	    #-DGLEW_LIBRARY="/home/simric/music/build/glew/build/lib/libGLEW.so"
 	    -DZLIB_LIBRARY_DEBUG="/lib/x86_64-linux-gnu/libz.so.1")
  else() 
 	set(cmake_args ${cmake_args}
@@ -75,15 +71,10 @@ if(UNIX)
               -DZLIB_LIBRARY_RELEASE="/usr/lib/libz.1.${LIB_EXT}"
               -DZLIB_LIBRARY_DEBUG="/usr/lib/libz.1.${LIB_EXT}")
 endif()
-
-
  
 ## #############################################################################
 ## Add external-project
 ## ############################################################################# 
-
-message("CMAKE CURRENT SOURCE DIR",${CMAKE_CURRENT_SOURCE_DIR}) 
-
 if(APPLE)
   set (cmake_args
   ${cmake_args}
@@ -92,37 +83,28 @@ endif(APPLE)
 
 ## cd ${CMAKE_CURRENT_SOURCE_DIR}/build/gvirtualXRay-prefix 
 set(patch_dir ${EP_PATH_SOURCE}/../../medInria-public/superbuild/patches)
-set(source_dir ${EP_PATH_SOURCE}/gVirtualXRay-1.0.0-Source)
-set(build_dir ${EP_PATH_SOURCE}/../build/gvirtualXRay)  
-
-message("dfmldf" ${build_dir})
+set(source_dir ${EP_PATH_SOURCE}/gvxr)
+set(build_dir ${EP_PATH_SOURCE}/../build/gvxr)
 
 ExternalProject_Add(gvirtualXRay
         PREFIX ${EP_PATH_SOURCE}
-	SOURCE_DIR ${EP_PATH_SOURCE}/${ep} 	
-	CMAKE_GENERATOR ${gen}
+        SOURCE_DIR ${EP_PATH_SOURCE}/${ep}
+        CMAKE_GENERATOR ${gen}
         CMAKE_GENERATOR_PLATFORM ${CMAKE_GENERATOR_PLATFORM}
         CMAKE_ARGS ${cmake_args}
 
-	DEPENDS ${${ep}_dependencies} 
-	DOWNLOAD_COMMAND curl -Lo gVirtualXRay-1.0.0.zip https://sourceforge.net/projects/gvirtualxray/files/1.0/gVirtualXRay-1.0.0-Source.zip/download
-	DOWNLOAD_NAME	gVirtualXRay-1.0.0.zip
-	PATCH_COMMAND 	unzip   ${EP_PATH_SOURCE}/src/gVirtualXRay-1.0.0.zip -d ${EP_PATH_SOURCE}/ && 
-	                cd ${source_dir} && 
-			patch ./cmake/CMakeLists.txt < ${patch_dir}/patchgVirtualXRay.txt  &&  
-			patch -i ${patch_dir}/polygonMeshHeaderPatch.txt  ${source_dir}/include/gVirtualXRay/PolygonMesh.h && 
-			patch -i ${patch_dir}/polygonMeshHeaderInlPatch.txt ${source_dir}/include/gVirtualXRay/PolygonMesh.inl    
-	CONFIGURE_COMMAND mkdir -p ${build_dir} && 
-	                  cd ${build_dir} && 
-	                  cmake  ${cmake_args} ${source_dir}/cmake/
-	BUILD_COMMAND  cd ${build_dir} &&  
-	               make install && 
-		       touch ${source_dir}/include/gVirtualXRay/gVirtualXRayConfig.h    ## might have to put 
-  	INSTALL_COMMAND ""
-  	UPDATE_COMMAND ""
-	)
-
-
+        DEPENDS ${${ep}_dependencies}
+        DOWNLOAD_COMMAND curl -Lo gVirtualXRay-1.1.0.zip https://sourceforge.net/projects/gvirtualxray/files/1.1/gVirtualXRay-1.1.0-Source.zip/download
+        DOWNLOAD_NAME   gVirtualXRay-1.1.0.zip
+        PATCH_COMMAND   unzip   ${EP_PATH_SOURCE}/src/gVirtualXRay-1.1.0.zip -d ${EP_PATH_SOURCE}/
+        CONFIGURE_COMMAND mkdir -p ${build_dir} &&
+                          cd ${build_dir} &&
+                          cmake  ${cmake_args} ${source_dir}
+        BUILD_COMMAND  cd ${build_dir} &&
+                       make install
+        INSTALL_COMMAND ""
+        UPDATE_COMMAND ""
+        )
 ## #############################################################################
 ## Set variable to provide infos about the project
 ## #############################################################################
