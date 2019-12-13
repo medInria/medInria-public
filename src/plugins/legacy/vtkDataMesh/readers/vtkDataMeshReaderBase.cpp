eef75437fa0b80bcb52126ef8f096f9da9184c04
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2019-12-13 16:34:00 +0100

    [vtk] copyrights

--- src/plugins/legacy/vtkDataMesh/readers/vtkDataMeshReaderBase.cpp
+++ src/plugins/legacy/vtkDataMesh/readers/vtkDataMeshReaderBase.cpp
@@ -1,3 +1,16 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2019. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
 #include "vtkDataMeshReaderBase.h"
 #include "vtkDataMeshWriterBase.h"
 
