ea1fa12a75e692e206b5d5f73ff999a7513f2033
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2020-03-24 17:07:23 +0100

    [cstddef] remove unused include & indentation

--- src/plugins/legacy/itkDataImage/readers/itkDataImageReaderCommand.cpp
+++ src/plugins/legacy/itkDataImage/readers/itkDataImageReaderCommand.cpp
@@ -11,8 +11,6 @@
 
 =========================================================================*/
 
-#include <cstddef> // For ITK 3.20 that does not define correctly ptrdiff_t
-
 #include <itkDataImageReaderCommand.h>
 
 #include <itkImageIOBase.h>
@@ -22,29 +20,28 @@
 namespace itk
 {
     void DataImageReaderCommand::Execute (Object *caller, const EventObject &event)
-    {
+    {       
         ImageIOBase *po = dynamic_cast<ImageIOBase *>(caller);
-
-        if (!po)
-            return;
         
-        if(typeid(event) == typeid(itk::ProgressEvent))
+        if(po && typeid(event) == typeid(itk::ProgressEvent))
         {
             if (m_Reader)
+            {
                 m_Reader->setProgress((int)(po->GetProgress()*100.0));
+            }
         }
     }
         
     void DataImageReaderCommand::Execute (const Object *caller, const EventObject &event)
     {
         ImageIOBase *po = dynamic_cast<ImageIOBase *>(const_cast<Object *>(caller) );
-        if (! po)
-            return;
         
-        if( typeid(event) == typeid ( itk::ProgressEvent  )  )
+        if(po && typeid(event) == typeid ( itk::ProgressEvent  )  )
         {
             if (m_Reader)
+            {
                 m_Reader->setProgress ( (int)(po->GetProgress()*100.0) );
+            }
         }
     }
 }
