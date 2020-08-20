5f60728ce5f2a1e23b934d01d136dcc9de04867a
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2020-08-20 15:35:16 +0200

    [Generic Workspace] use medSelectorWorkspace instead

--- src/layers/legacy/medCoreLegacy/gui/settingsWidgets/medStartupSettingsWidget.cpp
+++ src/layers/legacy/medCoreLegacy/gui/settingsWidgets/medStartupSettingsWidget.cpp
@@ -2,12 +2,14 @@
 
  medInria
 
- Copyright (c) INRIA 2013 - 2019. All rights reserved.
- See LICENSE.txt for details.
+ Copyright (c) INRIA 2013. All rights reserved.
 
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.
+ See LICENSE.txt for details in the root of the sources or:
+ https://github.com/medInria/medInria-public/blob/master/LICENSE.txt
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.
 
 =========================================================================*/
 
@@ -19,39 +21,16 @@
 #include <medSettingsManager.h>
 #include <medWorkspaceFactory.h>
 
-int retrieveGenericWorkSpace(QList<medWorkspaceFactory::Details*> pi_oListOfWorkspaceDetails)
-{
-    int iRes = -1;
-
-    bool bMatch = false;
-    medWorkspaceFactory::Details *poDetail = nullptr;
-    for (int i = 0; i < pi_oListOfWorkspaceDetails.size() && !bMatch; ++i)
-    {
-        poDetail = pi_oListOfWorkspaceDetails[i];
-        bMatch = poDetail->name == "Generic";
-        if (bMatch)
-        {
-            iRes = i;
-        }
-    }
-
-    return iRes;
-}
-
 class medStartupSettingsWidgetPrivate
 {
 public:
-    int m_iGenericWorkspaceIndex;
     QWidget *parent;
     QCheckBox *startInFullScreen;
-    QCheckBox *genericWorkspaceEnabled;
     QComboBox *defaultStartingArea;
 
     medStartupSettingsWidgetPrivate();
     ~medStartupSettingsWidgetPrivate();
 };
-
-
 
 medStartupSettingsWidgetPrivate::medStartupSettingsWidgetPrivate()
 {
@@ -67,13 +46,7 @@ medStartupSettingsWidget::medStartupSettingsWidget(QWidget *parent) : medSetting
     d->startInFullScreen = new QCheckBox(this);
     d->startInFullScreen->setToolTip(tr("Start application in full screen mode?"));
 
-    d->genericWorkspaceEnabled = new QCheckBox(this);
-    d->genericWorkspaceEnabled->setToolTip(tr("Enable generic workspace?"));
-
     QList<medWorkspaceFactory::Details*> workspaceDetails = medWorkspaceFactory::instance()->workspaceDetailsSortedByName(true);
-
-    d->m_iGenericWorkspaceIndex = retrieveGenericWorkSpace(workspaceDetails);
-    if (d->m_iGenericWorkspaceIndex!=-1)d->m_iGenericWorkspaceIndex += 3;
 
     d->defaultStartingArea = new QComboBox(this);
     d->defaultStartingArea->addItem(tr("Search"));
@@ -87,7 +60,6 @@ medStartupSettingsWidget::medStartupSettingsWidget(QWidget *parent) : medSetting
     }
     QFormLayout *layout = new QFormLayout;
     layout->addRow(tr("Fullscreen"), d->startInFullScreen);
-    layout->addRow(tr("Generic workspace enabled"), d->genericWorkspaceEnabled);
     layout->addRow(tr("Starting area"), d->defaultStartingArea);
     this->setLayout(layout);
 }
@@ -107,7 +79,6 @@ void medStartupSettingsWidget::read()
 {
     medSettingsManager *mnger = medSettingsManager::instance();
     d->startInFullScreen->setChecked(mnger->value("startup", "fullscreen").toBool());
-    d->genericWorkspaceEnabled->setChecked(mnger->value("startup", "genericWorkspace", false).toBool());
 
     //if nothing is configured then Homepage is the default area
     QString osDefaultStartingAreaName = mnger->value("startup", "default_starting_area", "Homepage").toString();
@@ -128,8 +99,6 @@ void medStartupSettingsWidget::read()
     {
         d->defaultStartingArea->setCurrentIndex(0);
     }
-
-    connect(d->genericWorkspaceEnabled, SIGNAL(stateChanged(int)), this, SLOT(genericWorkspaceState(int)));
 }
 
 bool medStartupSettingsWidget::write()
@@ -137,26 +106,6 @@ bool medStartupSettingsWidget::write()
     medSettingsManager *mnger = medSettingsManager::instance();
     mnger->setValue("startup", "fullscreen", d->startInFullScreen->isChecked());
     mnger->setValue("startup", "default_starting_area", d->defaultStartingArea->currentText());
-    mnger->setValue("startup", "genericWorkspace", d->genericWorkspaceEnabled->isChecked());
 
     return true;
-}
-
-void medStartupSettingsWidget::genericWorkspaceState(int pi_iState)
-{
-    if (d->m_iGenericWorkspaceIndex != -1)
-    {
-        if (pi_iState == Qt::Unchecked)
-        {
-            if (d->defaultStartingArea->currentText() == "Generic")
-            {
-                d->defaultStartingArea->setCurrentIndex(0);
-            }
-            d->defaultStartingArea->removeItem(d->m_iGenericWorkspaceIndex);
-        }
-        else
-        {
-            d->defaultStartingArea->insertItem(d->m_iGenericWorkspaceIndex, "Generic");
-        }
-    }
 }
--- src/layers/legacy/medCoreLegacy/gui/settingsWidgets/medStartupSettingsWidget.h
+++ src/layers/legacy/medCoreLegacy/gui/settingsWidgets/medStartupSettingsWidget.h
@@ -3,12 +3,14 @@
 
  medInria
 
- Copyright (c) INRIA 2013 - 2020. All rights reserved.
- See LICENSE.txt for details.
+ Copyright (c) INRIA 2013. All rights reserved.
 
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.
+ See LICENSE.txt for details in the root of the sources or:
+ https://github.com/medInria/medInria-public/blob/master/LICENSE.txt
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.
 
 =========================================================================*/
 
@@ -36,7 +38,4 @@ protected:
 
 private:
     medStartupSettingsWidgetPrivate *d;
-
-private slots:
-    void genericWorkspaceState(int pi_iState);
 };
