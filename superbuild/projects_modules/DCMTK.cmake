de8167e31d41f3bb030601f2b98987eeb74752c0
ci <ci@music-macos1015.cs1cloud.internal>
2020-10-30 14:20:48 +0100

    [patches] build_always 0 for patches

--- superbuild/projects_modules/DCMTK.cmake
+++ superbuild/projects_modules/DCMTK.cmake
@@ -113,7 +113,7 @@ ExternalProject_Add(${ep}
   CMAKE_ARGS ${cmake_args}
   DEPENDS ${${ep}_dependencies}
   INSTALL_COMMAND ""
-  BUILD_ALWAYS 1
+  BUILD_ALWAYS 0
   )
 
 ## #############################################################################
