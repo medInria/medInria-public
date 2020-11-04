7211809b221ac3315c98acda99a041aee700fb73
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2020-11-04 17:10:43 +0100

    [Conflicts] build always
    (cherry picked from commit 7557f8b02bb31693bab068d2f39ec2498f16b1bf)

--- superbuild/projects_modules/RPI.cmake
+++ superbuild/projects_modules/RPI.cmake
@@ -92,8 +92,8 @@ ExternalProject_Add(${ep}
   CMAKE_ARGS ${cmake_args}
   DEPENDS ${${ep}_dependencies}
   INSTALL_COMMAND ""
-  BUILD_ALWAYS 1
-  )  
+  BUILD_ALWAYS 0
+  ) 
   
 ## #############################################################################
 ## Set variable to provide infos about the project
