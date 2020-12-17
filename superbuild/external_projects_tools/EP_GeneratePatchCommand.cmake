cb7df08ca5563e222de796a2d1bf1637c5102c59
Florent LERAY <florent.leray@inria.fr>
2020-12-17 14:35:43 +0100

    1st step to improve EP_GeneratePatchCommand.cmake
    (cherry picked from commit 69bc3650597fa00ae4539d1a5077cea4b1eecfce)

--- superbuild/external_projects_tools/EP_GeneratePatchCommand.cmake
+++ superbuild/external_projects_tools/EP_GeneratePatchCommand.cmake
@@ -5,14 +5,34 @@
 function(ep_GeneratePatchCommand ep OutVar)
     find_program(GIT_BIN NAMES git)
     foreach (patch ${ARGN})
-        execute_process(COMMAND ${GIT_BIN} apply --ignore-whitespace  --check ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch}
-                        WORKING_DIRECTORY ${EP_PATH_SOURCE}/${ep}
-                        RESULT_VARIABLE PATCH_OK
-                        OUTPUT_QUIET
-                        ERROR_QUIET)
-        if (PATCH_OK EQUAL 0 OR NOT EXISTS ${EP_PATH_SOURCE}/${ep})
-            set(PATCHES_TO_APPLY ${PATCHES_TO_APPLY} ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch})
+        set(PATCHES_TO_APPLY_CUR ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch})
+        
+        if(EXISTS ${EP_PATH_SOURCE}/${ep})    	
+            execute_process(COMMAND ${GIT_BIN} apply --reverse --ignore-whitespace --check ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch}
+                            WORKING_DIRECTORY ${EP_PATH_SOURCE}/${ep}
+                            RESULT_VARIABLE PATCH_ALREADY_APPLIED
+                            OUTPUT_QUIET
+                            ERROR_QUIET)
+            				
+            if (PATCH_ALREADY_APPLIED EQUAL 0) #Like all OS system command 0 is Ok, not 0 is bad.
+            	# The patch is already applied
+            	message("The patch ${patch} for the external project ${ep} is already applied")
+            	set (PATCHES_TO_APPLY_CUR "") #Then erase PATCHES_TO_APPLY_CUR
+            else()
+                execute_process(COMMAND ${GIT_BIN} apply --ignore-whitespace --check ${CMAKE_SOURCE_DIR}/superbuild/patches/${patch}
+                                WORKING_DIRECTORY ${EP_PATH_SOURCE}/${ep}
+                                RESULT_VARIABLE PATCH_APPLICABLE
+                                OUTPUT_QUIET
+                                ERROR_QUIET)
+            	if(PATCH_APPLICABLE EQUAL 0) #Like all OS system command 0 is Ok, not 0 is bad.
+        		    #Do nothing the value is already set
+        		    message("The patch ${patch} will be applied on pre-existing ${EP_PATH_SOURCE}/${ep}")
+        		else()
+            		message(FATAL_ERROR "The patch ${patch} for the external project ${ep} is NOT APPLICABLE")
+            	endif()
+            endif()
         endif()
+		set(PATCHES_TO_APPLY ${PATCHES_TO_APPLY} ${PATCHES_TO_APPLY_CUR})
     endforeach()
 
     set(PATCH_COMMAND "")
