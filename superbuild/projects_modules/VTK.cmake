7dba9fa44523c29db866396330c64bc0bb077034
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2021-05-19 14:09:24 +0200

    [ITK5.3+VTK9.0] debug compilation

--- superbuild/projects_modules/VTK.cmake
+++ superbuild/projects_modules/VTK.cmake
@@ -68,31 +68,19 @@ set(cmake_args
   -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>  
   -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS_${ep}}
 
-  # VTK generic parameters 
+  # Generic parameters 
   -DVTK_BUILD_TESTING=OFF
   -DVTK_BUILD_DOCUMENTATION=OFF
   -DVTK_BUILD_EXAMPLES=OFF
   -DVTK_RENDERING_BACKEND=OpenGL2
 
-  #  Enabling modules for build
-  # Cf. https://vtk.org/doc/nightly/html/md__home_kitware_dashboards_buildbot_vtk_nightly-master-ike-linux-shared-release_doc_nightly_osm749dd663df9981384f2598c108aac3b0.html
-  #  YES: The module must be built.
-  #  NO: The module must not be built. If a YES module has a NO module in its dependency tree, an error is raised.
-  #  WANT: The module should be built. It will not be built, however, if it depends on a NO module.
-  #  DONT_WANT: The module doesn't need to be built. It will be built if a YES or WANT module depends on it.
-  #  DEFAULT: Look at other metadata to determine the status.
-
-  # TODO: setting to "YES" the modules, as done below in VTK_GROUP_ENABLE_Qt for instance does not work.
-  # I need to enter ccmake to change the value from DEFAULT to YES.
-
   # Qt
   -DVTK_GROUP_ENABLE_Qt=YES
   -DVTK_MODULE_ENABLE_VTK_GUISupportQt=YES
-  -DVTK_MODULE_ENABLE_VTK_GUISupportQtOpenGL=YES
   -DVTK_QT_VERSION=5
-  -DVTK_USE_OGGTHEORA_ENCODER:BOOL=ON # OGV Export
 
-  -DModule_vtkRenderingOSPRay:BOOL=${USE_OSPRay} 
+  # OGV Export
+  -DVTK_USE_OGGTHEORA_ENCODER:BOOL=ON
   )
   
 set(cmake_cache_args
@@ -101,6 +89,7 @@ set(cmake_cache_args
 
 if(USE_OSPRay)
     list(APPEND cmake_cache_args
+        -DVTK_MODULE_ENABLE_VTK_RenderingOSPRay=YES
         -Dospray_DIR=${ospray_DIR}
         -DOSPRAY_INSTALL_DIR=${OSPRAY_INSTALL_DIR}
     )
@@ -110,7 +99,7 @@ endif()
 if(${USE_FFmpeg})
     list(APPEND cmake_args
         # FFMPEG
-        -DModule_vtkIOFFMPEG:BOOL=ON
+        -DVTK_MODULE_ENABLE_VTK_IOFFMPEG=YES
         -DFFMPEG_ROOT:STRING=${EP_PATH_BUILD}/ffmpeg
         -DFFMPEG_INCLUDE_DIR:STRING=${EP_PATH_BUILD}/ffmpeg/include/
 
@@ -129,12 +118,6 @@ if(${USE_FFmpeg})
         -DFFMPEG_LIBSWSCALE_LIBRARIES:STRING=${EP_PATH_BUILD}/ffmpeg/lib/libswscale.${extention}
     )
 endif()
-
-## #############################################################################
-## Check if patch has to be applied
-## #############################################################################
-
-#ep_GeneratePatchCommand(${ep} ${ep}_PATCH_COMMAND VTK.patch)
 
 ## #############################################################################
 ## Add external-project
@@ -158,6 +141,7 @@ ExternalProject_Add(${ep}
   CMAKE_CACHE_ARGS ${cmake_cache_args}
   DEPENDS ${${ep}_dependencies}
   INSTALL_COMMAND ""
+  BUILD_ALWAYS 1
   )
 ## #############################################################################
 ## Set variable to provide infos about the project
