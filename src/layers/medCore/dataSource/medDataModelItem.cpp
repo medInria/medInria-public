11ce7d33aa11afef34130300a1fa55fca49e7f1b
Florent LERAY <florent.leray@inria.fr>
2021-08-31 15:02:52 +0200

    Implement part of medDataModelItem for location

--- src/layers/medCore/dataSource/medDataModelItem.cpp
+++ src/layers/medCore/dataSource/medDataModelItem.cpp
@@ -1,21 +1,75 @@
 #include <medDataModelItem.h>
 
+#include <QVariant>
+#include <QList>
+#include <QVector>
+#include <QString>
+
+//#include <medDataModelElement.h>
+
+class medDataModelItemPrivate
+{
+public:
+    medDataModelItem * parentItem;
+    QList<medDataModelItem *> childItems;
+
+    QList<QVariant> itemData;
+
+    unsigned int uiLevel;
+};
+
+
+
+medDataModelItem::medDataModelItem() : d(new medDataModelItemPrivate())
+{
+}
+
+medDataModelItem::~medDataModelItem()
+{
+    delete d;
+}
+
+
+
+
 int medDataModelItem::childCount()
 {
-    return 0;
+    return d->childItems.size();
 }
 
 unsigned int medDataModelItem::childLevel()
 {
-    return 0;
+    return d->uiLevel+1;
 }
 
 int medDataModelItem::row() const
 {
-    return 0;
+    int iRowRes = 0;
+
+    if (d->parentItem)
+    {
+        iRowRes = d->parentItem->d->childItems.indexOf(const_cast<medDataModelItem*>(this));
+    }
+
+    return iRowRes;
 }
 
 medDataModelItem * medDataModelItem::parent()
 {
-    return nullptr;
+    return d->parentItem;
+}
+
+
+
+
+
+
+medDataModelItem * medDataModelItem::child(int row)
+{
+    return d->childItems.value(row);
+}
+
+QVariant medDataModelItem::data(int column) const
+{
+    return d->itemData.value(column);
 }
