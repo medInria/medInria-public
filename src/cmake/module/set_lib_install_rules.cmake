427a45f8b98d1f8699135338d125db2f00147b1d
fcollot <fcollot@users.noreply.github.com>
2022-04-08 14:54:05 +0200

    Resource libraries (#994)
    * mac_packager: fix plugin install names
    
    * ApplePackaging.cmake: @ONLY in configuration of mac_packager
    
    * fix comment error (and wrap to the standard 80 char width)
    
    * Resource libraries

--- src/cmake/module/set_lib_install_rules.cmake
+++ src/cmake/module/set_lib_install_rules.cmake
@@ -11,64 +11,76 @@
 #
 ################################################################################
 
-macro(set_lib_install_rules_generic target dest)
+function(set_lib_install_rules target)
 
 ################################################################################
 #
-# Usage: set_lib_install_rules(target, header1, header2, header3 ...)
-# set rules for the library designed by the target, and add all the additional
-# header to {CMAKE_PREFIX_INSTALL}/include during install step.
+# Usage: set_lib_install_rules(target [RESOURCE] [HEADERS [headers...]])
+#
+# Set rules for a library target.
+#
+# The options are:
+#
+# RESOURCE
+#     Indicate that this is a resource library. Resource libraries are shared
+#     librairies that are not dependencies (direct or indirect) of the
+#     executable. An example of this are Python extension modules, which are not
+#     linked into the executable but loaded by the embedded Python's import
+#     mechanism.
+#
+# HEADERS
+#     Add headers to '{CMAKE_PREFIX_INSTALL}/include' during the install step.
 #
 ################################################################################
 
-get_property(GENERATOR_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+    cmake_parse_arguments(PARSE_ARGV 1 "ARG" "RESOURCE" "" "HEADERS")
+    get_property(GENERATOR_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 
-if(${GENERATOR_MULTI_CONFIG})
-  set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/bin)
-  set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/bin)
-  set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/bin)
-  set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/bin)
-                                                                                        
-  set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/lib)
-  set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/lib)
-  set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/lib)
-  set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/lib)
-                                                                                        
-  set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/lib)
-  set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/lib)
-  set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/lib)
-  set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/lib)
-else()                                                                                  
-  set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY                ${dest}/bin)
-  set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY                ${dest}/lib)
-  set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY                ${dest}/lib)
-endif()
+    if (medInria_DIR)
+        set(dest ${medInria_DIR})
+    else()
+        set(dest ${CMAKE_BINARY_DIR})
+    endif()
 
-install(TARGETS ${target}
-  RUNTIME DESTINATION bin
-  LIBRARY DESTINATION lib
-  ARCHIVE DESTINATION lib
-  FRAMEWORK DESTINATION lib
-  RESOURCE DESTINATION resources/${target}
-  )
+    set(shared_lib_dir lib$<$<AND:$<BOOL:${ARG_RESOURCE}>,$<PLATFORM_ID:Darwin>>:/resource_libs>)
+
+    if(${GENERATOR_MULTI_CONFIG})
+        set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/bin)
+        set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/bin)
+        set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/bin)
+        set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/bin)
+
+        set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/lib)
+        set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/lib)
+        set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/lib)
+        set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/lib)
+
+        set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_DEBUG          ${dest}/${platformType}Debug/${shared_lib_dir})
+        set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_RELEASE        ${dest}/${platformType}Release/${shared_lib_dir})
+        set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_MINSIZEREL     ${dest}/${platformType}MinSizeRel/${shared_lib_dir})
+        set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO ${dest}/${platformType}RelWithDebInfo/${shared_lib_dir})
+    else()
+        set_target_properties( ${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY                ${dest}/bin)
+        set_target_properties( ${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY                ${dest}/lib)
+        set_target_properties( ${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY                ${dest}/${shared_lib_dir})
+    endif()
+
+    install(TARGETS ${target}
+        RUNTIME DESTINATION lib
+        LIBRARY DESTINATION ${shared_lib_dir}
+        ARCHIVE DESTINATION lib
+        FRAMEWORK DESTINATION ${shared_lib_dir}
+        RESOURCE DESTINATION resources/${target}
+        )
 
 ## #############################################################################
-## Add header wich have to be exposed in the include dir of the install tree
+## Add headers wich have to be exposed in the include dir of the install tree
 ## #############################################################################
 
-if(${ARGC} GREATER 2)
-  install(FILES ${ARGN}
-    DESTINATION include/${target}
-    )
-endif()
+    if(ARG_HEADERS)
+        install(FILES ${ARG_HEADERS}
+            DESTINATION include/${target}
+            )
+    endif()
 
-endmacro()
-
-
-macro(set_lib_install_rules target)
-  set_lib_install_rules_generic(${target} ${CMAKE_BINARY_DIR} ${ARGN})
-endmacro()
-
-macro(set_lib_install_rules_external target)
-  set_lib_install_rules_generic(${target} ${medInria_DIR} ${ARGN})
-endmacro()
+endfunction()
