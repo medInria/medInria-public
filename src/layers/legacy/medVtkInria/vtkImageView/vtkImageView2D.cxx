937a10ae2acb79f6959de604de29ef8d1b57f5b0
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2023-06-07 10:25:52 +0200

    [MemLeak] solve a graphic pb in Paint

--- src/layers/legacy/medVtkInria/vtkImageView/vtkImageView2D.cxx
+++ src/layers/legacy/medVtkInria/vtkImageView/vtkImageView2D.cxx
@@ -1798,6 +1798,7 @@ void vtkImageView2D::SetInputCommon(vtkAlgorithmOutput* pi_poVtkAlgoOutput, int 
 
         this->SetCurrentLayer(layer);
         this->Slice = this->GetSliceForWorldCoordinates (this->CurrentPoint);
+        this->UpdateDisplayExtent();
         this->UpdateSlicePlane();
         this->InvokeEvent (vtkImageView2D::SliceChangedEvent);
 
