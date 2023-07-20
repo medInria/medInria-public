981a13cef21a505192c524fe823de0a63e00daec
LFLCH <leo.filoche@inria.fr>
2023-07-20 14:29:01 +0200

    fixing spaces issues from the merge

--- src/layers/medCore/source/operating/medSourceHandler.h
+++ src/layers/medCore/source/operating/medSourceHandler.h
@@ -73,7 +73,7 @@ signals:
 	void getAsyncStatus(QString const /*sourceId*/, int /*requestId*/, medAbstractSource::eRequestStatus /*status*/);
 	void sourceAdded(QString const /*sourceId*/);
 	void sourceRemoved(QString const /*sourceId*/);
-    void connectionDisconnection(QString, bool);	void connectionDisconnection(QString, bool);
+	void connectionDisconnection(QString, bool);
 
 public:
 	// ////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,12 +90,11 @@ public:
 
 
 public slots:
-    void addSource(medAbstractSource* pi_source);
-    void removeSource(medAbstractSource* pi_source);
+	void addSource(medAbstractSource* pi_source);
+	void removeSource(medAbstractSource* pi_source);
 
-    void sourceIsOnline(QString sourceIntanceId);/* REDO */
-    void progress(int pi_iRequest, medAbstractSource::eRequestStatus status);
-
+	void progress(int pi_iRequest, medAbstractSource::eRequestStatus status);
+	void connectionStatus(bool status);
 
 private:
 	medAbstractSource * getSource(QString const pi_sourceInstanceId);
