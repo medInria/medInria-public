0a2030124424eacc6a71613e58971f42387d1423
Léo Filoche <leofiloche.wk@gmail.com>
2023-09-03 19:40:59 +0200

    comments on internal  header files

--- src/plugins/legacy/medShanoir/internal/LocalInfo.h
+++ src/plugins/legacy/medShanoir/internal/LocalInfo.h
@@ -11,14 +11,28 @@ class RequestManager;
 
 using levelMinimalEntries = medAbstractSource::levelMinimalEntries;
 
+/**
+ * @class LocalInfo
+ * @brief This class is used to store the information that do not require a request to the shanoir server.
+*/
 class LocalInfo : public QObject
 {
 	Q_OBJECT
 
 public:
+
+//////////////////////////
+////  Constructor/Destructor
+//////////////////////////
+
     LocalInfo(medShanoir * parent);
+    
     ~LocalInfo();
 
+//////////////////////////
+////  MedShanoir methods
+//////////////////////////  
+    
     bool initialization(QString const &pi_instanceId);
 
     bool setInstanceName(QString const &pi_instanceName);
@@ -62,11 +76,34 @@ public:
     bool commitData(QVariant data, medAbstractSource::levelMinimalEntries &pio_minimalEntries, unsigned int pi_uiLevel, QString parentKey);
 
 public:
+
+////////////////////////////
+//// internal logic methods
+////////////////////////////
+
+    /**
+     * @return the username that is entered for the plugin instance.
+    */
     QString getUsername();
+
+    /**
+     * @return the password that is entered for the plugin instance.
+    */
     QString getPassword();
+    
+    /**
+     * @return the hostname that is entered for the plugin instance.
+    */
     QString getDomain();
+
+    /**
+     * @return the root URL of the shanoir server for requests creation.
+    */
     QString getBaseURL();
 
+    /**
+     * @return the path to the storage folder of the plugin instance.
+    */
 	QString getStoragePath();
 
 
@@ -75,9 +112,11 @@ private:
     const QStringList s_level_names = {"study", "subject", "examination", "dataset-acquisition", "dataset", "processingDataset", "processedDataset"};
 
     // dynamic variables 
-    QString m_instance_id;
-	QString m_instance_name;
-	medStringParameter m_username;
-	medStringParameter m_password;
-	medStringParameter m_hostname;
+    QString m_instance_id;    // id of the plugin instance
+	QString m_instance_name;  // name of the plugin instance
+
+    // plugin instance parameters
+	medStringParameter m_username; // username of the plugin instance
+	medStringParameter m_password; // password of the plugin instance
+	medStringParameter m_hostname; // hostname of the plugin instance
 };
--- src/plugins/legacy/medShanoir/internal/SyncNetwork.h
+++ src/plugins/legacy/medShanoir/internal/SyncNetwork.h
@@ -1,11 +1,12 @@
 #include <QObject>
-
 #include <QEventLoop>
 #include <QUuid>
 #include <QVariant>
 #include <QVector>
 #include <QList>
 #include <QJsonArray>
+
+#include <medAbstractSource.h>
 
 #include <RequestManager.h>
 #include <Levels.h>
@@ -14,54 +15,65 @@ class Authenticator;
 class medShanoir;
 class LocalInfo;
 
-#include <medAbstractSource.h>
-
 using levelMinimalEntries = medAbstractSource::levelMinimalEntries;
 using datasetAttributes = medAbstractSource::datasetAttributes;
 using entryTypeLocal = medAbstractSource::entryType;
 
-
+/**
+ * @class SyncNetwork
+ * @brief This class is used to make all the operation that require a request to the shanoir server in a synchronous way by the plugin. 
+ * @details 
+ * Synchroneous means that the plugin will wait for the request to be finished before continuing its execution.
+ * A timeout management also implemented to avoid the plugin to wait forever for a request to be finished.
+ * 
+*/
 class SyncNetwork : public QObject
 {
 	Q_OBJECT
 
 public:
+//////////////////////////
+////  Constructor/Destructor
+//////////////////////////
+
     SyncNetwork(medShanoir * parent,  LocalInfo *info, Authenticator * authent, RequestManager * requestMgr);
-    ~SyncNetwork();
+    
+	~SyncNetwork();
 
+//////////////////////////
+////  MedShanoir methods
+//////////////////////////  
 
-	/* ***********************************************************************/
-	/* *************** Get elements data *************************************/
-	/* ***********************************************************************/
 	QList<levelMinimalEntries>    getMinimalEntries(unsigned int pi_uiLevel, QString parentKey);
+
 	QList<QMap<QString, QString>> getMandatoryAttributes(unsigned int pi_uiLevel, QString parentKey);
 
-	// Not implemented yet
-	//bool getAdditionalAttributes(unsigned int pi_uiLevel, QString key, datasetAttributes &po_attributes);
+	//bool getAdditionalAttributes(unsigned int pi_uiLevel, QString key, datasetAttributes &po_attributes); // it may be interesting to implement this function in the future
 
-
-	/* ***********************************************************************/
-	/* *************** Get data          *************************************/
-	/* ***********************************************************************/
 	QVariant getDirectData(unsigned int pi_uiLevel, QString key);
 
-	/* ***********************************************************************/
-	/* *************** Store data          ***********************************/
-	/* ***********************************************************************/
 	bool addDirectData(QVariant data, levelMinimalEntries &pio_minimalEntries, unsigned int pi_uiLevel, QString parentKey);
 	
-	//bool createPath(QList<levelMinimalEntries> &pio_path, datasetAttributes const &pi_attributes, unsigned int pi_uiLevel = 0, QString parentKey = "");
+
 	bool createFolder(levelMinimalEntries &pio_minimalEntries, datasetAttributes const &pi_attributes, unsigned int pi_uiLevel, QString parentKey);
 
-	// The following functions have no effect
+	// The following functions have no reality in shanoir, and so in the plugin
+	//bool createPath(QList<levelMinimalEntries> &pio_path, datasetAttributes const &pi_attributes, unsigned int pi_uiLevel = 0, QString parentKey = "");
 	//bool alterMetaData(datasetAttributes const &pi_attributes, unsigned int pi_uiLevel, QString key);
 	//bool getThumbnail(QPixmap &po_thumbnail, unsigned int pi_uiLevel, QString key);
 	//bool setThumbnail(QPixmap &pi_thumbnail, unsigned int pi_uiLevel, QString key);
 
 public slots:
+	/**
+	 * @brief slot received from the request manager updating a request progression status
+	 */
 	void syncRequestSlot(QUuid netReqId, QByteArray payload, QJsonObject headers, int statusOrHttpCode);
 
 signals:
+//////////////////////////
+////  signals emitted for sending a request with the request manager
+//////////////////////////  
+
 	void syncGet(QUuid, QNetworkRequest);
 	void syncPost(QUuid, QNetworkRequest, QByteArray); 
 	void syncPostMulti(QUuid, QNetworkRequest, QHttpMultiPart *);
@@ -69,7 +81,11 @@ signals:
 	void syncAbort(QUuid);
 
 private:
+//////////////////////////
+////  representations for synchronous requests management
+//////////////////////////
 
+	// represents the type of request that can be made to the shanoir server
 	enum SyncRequestType : int
 	{
 		searchWithSolr = -6,
@@ -84,6 +100,7 @@ private:
 		addFolder = 4
 	};
 
+	// synchroneous request representation
 	struct SyncRequest
 	{
 		QEventLoop* waiter;
@@ -92,25 +109,42 @@ private:
 		bool upToDate;
 	};
 
-	QMap<SyncRequestType, int> requestsDurations; // In ms. -1 if it should not have a timer
-
 private:
-	//     WAITERS
+//////////////////////////
+//// request sending methods
+//////////////////////////
+
+	//     WAITERS : methods that send a request and wait for a it to be finished
 	QUuid waitGetResult(SyncRequestType type, QNetworkRequest &req);
-	void updateRequestState(SyncRequestType &type, const QUuid &netReqId);
 	QUuid waitPostResult(SyncRequestType type, QNetworkRequest &req, QByteArray &postData);
 	QUuid waitPostMultiResult(SyncRequestType type, QNetworkRequest &req, QHttpMultiPart *postData);
 	QUuid waitPutResult(SyncRequestType type, QNetworkRequest &req, QByteArray &postData);
 
-	// TREE-VIEW RETRIEVING FUNCTIONS
-	//      GETTERS
-	QList<StudyOverview> getStudies();
-	QList<Examination>   getExaminations(int studyId, int subjectId);
+	/**
+	 * @param type type of the netReqId request
+	 * @param netReqId id of a request 
+	 * @brief keeps the given request up to date, by checking if any new interaction has been made with the server since the last time the request was checked. If it is not the case, the request is aborted.
+	*/
+	void updateRequestState(SyncRequestType &type, const QUuid &netReqId);
+
+	/**
+	 * @brief properly manage the death of a request, by removing it from the request map and showing an error message if needed
+	 * @param netReqId the id of a finished request
+	 */
+	void manageRequestDeath(QUuid netReqId);
 
 public:
+//////////////////////////
+////  Tree-view elements retrieving methods
+//////////////////////////
+
+	//      GETTERS : methods that send a request and parse the response to return the wanted data
+	QList<StudyOverview> getStudies();
+	QList<Examination>   getExaminations(int studyId, int subjectId);
 	Study                getStudyDetails(int studyId);
-	DatasetDetails       getDatasetDetails(int studyId);  // not used for the tree-view but for the upload of data
-	QJsonObject          getDatasetProcessing(int id); // not used for the tree-view but for the upload of data
+	DatasetDetails       getDatasetDetails(int studyId);  
+	QJsonObject          getDatasetProcessing(int id); 
+
 	/**
 	 *  not used for now. Everything is in place to recuperate a result from a solr request.
 	 * modify the signature to adapt to the request you want to make (see writeGetSolrRequest from ShanoirRequestPreparation.h)
@@ -118,35 +152,55 @@ public:
 	QJsonObject                 applySolrRequest();   
 
 private:
-	//      PARSERS
+
+	//      PARSERS : methods that parse the response of a request to return the wanted data
 	QList<StudyOverview>      parseStudies(QJsonArray studies_response);
 	QList<Examination>        parseExaminations(QJsonArray examinations_response);
 	QList<DatasetAcquisition> parseDatasetAcquisitions(QJsonArray dataset_acquisitions_response);
 	QList<Dataset>            parseDatasets(QJsonArray datasets_response);
 	QList<DatasetProcessing>  parseDatasetProcessings(QJsonArray processings_response);
 	Study                     parseStudy(QJsonObject &study_response);
-
 	DatasetDetails            parseDatasetDetails(QJsonObject &dataset_response);
 
-	//      CASTERS
-	QList<levelMinimalEntries> getRootMinimalEntries                ();
-	QList<levelMinimalEntries> getStudyMinimalEntries               (QString id);
-	QList<levelMinimalEntries> getSubjectMinimalEntries             (QString id);
-	QList<levelMinimalEntries> getExaminationMinimalEntries			(QString id);
-	QList<levelMinimalEntries> getDatasetAcquisitionMinimalEntries	(QString id);
-	QList<levelMinimalEntries> getDatasetMinimalEntries				(QString id);
-	QList<levelMinimalEntries> getProcessingDatasetMinimalEntries	(QString id);
+	//      CASTERS : methods that retrieve the tree-view children elements of a given parent id
+	/**
+	 * @brief  retrieve a list of studies
+	*/
+	QList<levelMinimalEntries> getRootMinimalEntries                ();           
+	/**
+	 * @brief retrieve a list of subjects contained by the given study
+	*/
+	QList<levelMinimalEntries> getStudyMinimalEntries               (QString id); 
+	/**
+	 * @brief  retrieve a list of examinations contained by the given subject
+	*/
+	QList<levelMinimalEntries> getSubjectMinimalEntries             (QString id); 
+	/**
+	 * @brief  retrieve a list of datasets-acquisitions contained by the given examination
+	*/
+	QList<levelMinimalEntries> getExaminationMinimalEntries			(QString id); 
+	/**
+	 * @brief  retrieve a list of datasets contained by the given dataset-acquisition
+	*/
+	QList<levelMinimalEntries> getDatasetAcquisitionMinimalEntries	(QString id); 
+	/**
+	 * @brief retrieve a list of dataset processings contained by the given dataset
+	*/
+	QList<levelMinimalEntries> getDatasetMinimalEntries				(QString id); 
+	/**
+	 * @brief retrieve a list of processed datasets contained by the given processing dataset
+	*/
+	QList<levelMinimalEntries> getProcessingDatasetMinimalEntries	(QString id); 
 
 
-	void manageRequestDeath(QUuid netReqId);
 
 private:
-	LocalInfo           * m_info;
-	Authenticator       * m_authent;
-	QAtomicInt            m_medReqId;
+	LocalInfo           * m_info;    // pointer to the plugin instance info
+	Authenticator       * m_authent; // pointer to the plugin instance authenticator
+	
+	QMap<SyncRequestType, int> requestsDurations; // duration of each request type (in milliseconds). The value if -1 if it should not have a timer
 
-	QMap<QUuid, SyncRequest> m_requestMap;
+	QMap<QUuid, SyncRequest> m_requestMap; // map of the active requests sent by the syncNetwork object.
 
-
-	QStringList m_filesToRemove;
+	QStringList m_filesToRemove; // list of the files to remove when the plugin instance is closed
 };
