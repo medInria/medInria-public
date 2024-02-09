b7e1b0ad47a00d899bf483e9f00703be8162624c
FLORENT LERAY <florent.leray@inria.fr>
2024-02-09 15:16:14 +0100

    Remove implementation of constructor because we have a constructor by inheritance ```C++  using QStyledItemDelegate::QStyledItemDelegate; ```

--- src/layers/medCoreGui/source/widgets/medVirtualRepresentationItemDelegate.cpp
+++ src/layers/medCoreGui/source/widgets/medVirtualRepresentationItemDelegate.cpp
@@ -2,9 +2,6 @@
 
 #include <QPainter>
 
-medVirtualRepresentationItemDelegate::medVirtualRepresentationItemDelegate()
-{
-}
 
 void medVirtualRepresentationItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
