670d8cf0cda59bbf8eda4a843bafacb3f1db2267
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2024-07-09 14:33:07 +0200

    [VTK9] remove include_directories()

--- src/plugins/legacy/reformat/medResliceViewer.h
+++ src/plugins/legacy/reformat/medResliceViewer.h
@@ -1,14 +1,14 @@
 #pragma once
 /*=========================================================================
 
- medInria
+medInria
 
- Copyright (c) INRIA 2019. All rights reserved.
- See LICENSE.txt for details.
+Copyright (c) INRIA 2013 - 2020. All rights reserved.
+See LICENSE.txt for details.
 
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
 
 =========================================================================*/
 
